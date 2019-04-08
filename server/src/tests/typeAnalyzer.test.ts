6b643718e11f7aef9ecf66ef961f32e25fbf563d
mhzed <minhongz@gmail.com>
2019-04-08 10:19:30 +0800

    Merge tests

--- server/src/tests/typeAnalyzer.test.ts
+++ server/src/tests/typeAnalyzer.test.ts
@@ -209,3 +209,18 @@ test('Tuples1', () => {
     assert.equal(analysisResults.length, 1);
     assert.equal(analysisResults[0].errors.length, 4);
 });
+
+test('NamedTuple', () => {
+    let [analysisResult] = TestUtils.typeAnalyzeSampleFiles(['namedtuple.py']);
+
+    assert.equal(analysisResult.errors.length, 0);
+    assert.equal(analysisResult.warnings.length, 0);
+});
+
+test('NamedTuple field name error', () => {
+    let [analysisResult] = TestUtils.typeAnalyzeSampleFiles(['namedtuple2.py']);
+
+    assert.equal(analysisResult.errors.length, 1);
+    assert.ok(/start with _/.test(analysisResult.errors[0].message));
+    assert.equal(analysisResult.warnings.length, 0);
+});
