79ed26ba634994809ceb00fa3a7b52ced926b39b
mhzed <minhongz@gmail.com>
2019-04-10 11:13:44 +0800

    fix typing.NamedTuple check

---
+++ server/src/tests/samples/namedtuple3.py
@@ -0,0 +1,12 @@
+# This sample validates the Python 3.7 data class feature, ensuring that
+# NamedTuple must be a direct base class.
+
+from typing import NamedTuple
+
+class Parent(NamedTuple):
+    pass
+
+class DataTuple2(Parent):
+    id: int
+
+data = DataTuple2(id=1)
