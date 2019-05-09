32771db26341fdf194939d45bbbd322a59157ffb
Eric Traut <erictr@microsoft.com>
2019-05-09 14:48:56 -0700

    Revert "Small tweaks for coding convention consistency."
    This reverts commit 2769c693747edc9e853819e8314e7f5bd995bbe7.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -10,23 +10,21 @@
 */
 
 import * as path from 'path';
-import { ExtensionContext, workspace as Workspace, TextDocument,
-	OutputChannel, window as Window } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions,
-	TransportKind } from 'vscode-languageclient';
+import { ExtensionContext, workspace as Workspace, TextDocument, OutputChannel, window as Window } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient';
 import { ProgressReporting } from './progress';
 
-const clients = new Map<string, LanguageClient>();
+let clients: Map<string, LanguageClient> = new Map();
 
 export function activate(context: ExtensionContext) {
-	const outputChannel: OutputChannel = Window.createOutputChannel('pyright');
-	const serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
-	const debugOptions = { execArgv: ["--nolazy", "--inspect=6600"] };
+	let outputChannel: OutputChannel = Window.createOutputChannel('pyright');
+	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+	let debugOptions = { execArgv: ["--nolazy", "--inspect=6600"] };
 
 	// If the extension is launched in debug mode, then the debug server options are used.
 	// Otherwise the run options are used.
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
+	let serverOptions: ServerOptions = {
+		run : { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
 	}
 
@@ -34,15 +32,14 @@ export function activate(context: ExtensionContext) {
 		if (!(document.languageId === 'python' && document.uri.scheme === 'file')) {
 			return;
 		}
-
-		const folder = Workspace.getWorkspaceFolder(document.uri);
+		let folder = Workspace.getWorkspaceFolder(document.uri);
 		if (!folder || clients.has(folder.uri.toString())) {
 			return;
 		}
 
 		// Create the language client and start the client.
 		// Options to control the language client
-		const clientOptions: LanguageClientOptions = {
+		let clientOptions: LanguageClientOptions = {
 			// Register the server for python source files.
 			documentSelector: [{
 				scheme: 'file',
@@ -56,9 +53,8 @@ export function activate(context: ExtensionContext) {
 			workspaceFolder: folder,
 			outputChannel: outputChannel
 		}
-		const languageClient = new LanguageClient('python', 'Pyright',
-			serverOptions, clientOptions);
-		const disposable = languageClient.start();
+		let languageClient = new LanguageClient('python', 'Pyright', serverOptions, clientOptions);
+		let disposable = languageClient.start();
 
 		// Push the disposable to the context's subscriptions so that the 
 		// client can be deactivated on extension deactivation.
@@ -70,14 +66,11 @@ export function activate(context: ExtensionContext) {
 
 		clients.set(folder.uri.toString(), languageClient);
 	};
-
 	Workspace.onDidOpenTextDocument(detectDoc);
 	Workspace.textDocuments.forEach(detectDoc);
-
 	Workspace.onDidChangeWorkspaceFolders((event) => {
 		for (let folder of event.removed) {
-			const client = clients.get(folder.uri.toString());
-
+			let client = clients.get(folder.uri.toString());
 			if (client) {
 				clients.delete(folder.uri.toString());
 				client.stop();
@@ -87,11 +80,9 @@ export function activate(context: ExtensionContext) {
 }
 
 export function deactivate(): Thenable<void> {
-	const promises: Thenable<void>[] = [];
-
+	let promises: Thenable<void>[] = [];
 	for (let client of clients.values()) {
 		promises.push(client.stop());
 	}
-
 	return Promise.all(promises).then(() => undefined);
 }
