35761a360fae380156e13a62ab9e8cecc0669fe0
Eric Traut <erictr@microsoft.com>
2019-11-16 16:42:58 -0800

    Reduced time delay before checks from 1.5 sec to 1.0 sec.

--- server/src/analyzer/service.ts
+++ server/src/analyzer/service.ts
@@ -809,7 +809,7 @@ export class AnalyzerService {
         // How long has it been since the user interacted with the service?
         // If the user is actively typing, back off to let him or her finish.
         const timeSinceLastUserInteractionInMs = Date.now() - this._lastUserInteractionTime;
-        const minBackoffTimeInMs = 1500;
+        const minBackoffTimeInMs = 1000;
 
         // We choose a small non-zero value here. If this value
         // is too small (like zero), the VS Code extension becomes
