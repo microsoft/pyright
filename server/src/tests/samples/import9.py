b039f6c118f1e614fb8670de616c5e7253dc1e73
Eric Traut <erictr@microsoft.com>
2020-01-02 01:39:45 -0700

    Fixed error in comment.

--- server/src/tests/samples/import9.py
+++ server/src/tests/samples/import9.py
@@ -1,6 +1,6 @@
 # This sample tests support for PEP 562's __getattr__ function.
 
-# This should not generate an error because import9 has
+# This should not generate an error because import8 has
 # a __getattr__ method.
 from .import8 import foo
 
