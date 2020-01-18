0ea688b77e00725c58230c8dbb3271640ef678e7
HeeJae Chang <hechang@microsoft.com>
2020-01-17 17:05:26 -0800

    always escape / in regex

--- server/src/common/pathUtils.ts
+++ server/src/common/pathUtils.ts
@@ -572,7 +572,7 @@ export function getFileSpec(rootPath: string, fileSpec: string): FileSpec {
 }
 
 export function getRegexEscapedSeparator() {
-    return path.sep === '/' ? '/' : '\\\\';
+    return path.sep === '/' ? '\/' : '\\\\';
 }
 
 /**
