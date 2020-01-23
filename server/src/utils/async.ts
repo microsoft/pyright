73035110068913289c0f08bcffddaf14c667ad79
MikhailArkhipov <mikhaila@microsoft.com>
2020-01-23 11:58:46 -0800

    Style

--- server/src/utils/async.ts
+++ server/src/utils/async.ts
@@ -1,17 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+/* eslint-disable @typescript-eslint/no-empty-function */
+
 // tslint:disable-next-line:interface-name
 declare interface Promise<T> {
-/**
- * Catches task error and ignores them.
- */
+    // Catches task error and ignores them.
     ignoreErrors(): void;
 }
 
-/**
- * Explicitly tells that promise should be run asynchonously.
- */
+// Explicitly tells that promise should be run asynchonously.
 Promise.prototype.ignoreErrors = function<T>(this: Promise<T>) {
     // tslint:disable-next-line:no-empty
     this.catch(() => {});
