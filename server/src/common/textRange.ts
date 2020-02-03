a1e3efe80eb02864f715ab5070ecf3d48ff2faf6
HeeJae Chang <hechang@microsoft.com>
2020-02-03 12:39:02 -0800

    re-arrange stuff to make vscode-lsp and pyright to use similar pattern to reduce confusion

--- server/src/common/textRange.ts
+++ server/src/common/textRange.ts
@@ -6,7 +6,7 @@
 *
 * Specifies the range of text within a larger string.
 */
-import { Position, Range } from 'vscode-languageserver';
+import * as lsp from 'vscode-languageserver';
 
 export interface TextRange {
     start: number;
@@ -68,6 +68,11 @@ export interface LineAndColumn {
     character: number;
 }
 
+export interface LineAndColumnRange {
+    start: LineAndColumn;
+    end: LineAndColumn;
+}
+
 export function comparePositions(a: LineAndColumn, b: LineAndColumn) {
     if (a.line < b.line) {
         return -1;
@@ -88,11 +93,6 @@ export function getEmptyPosition(): LineAndColumn {
     };
 }
 
-export interface LineAndColumnRange {
-    start: LineAndColumn;
-    end: LineAndColumn;
-}
-
 export function doRangesOverlap(a: LineAndColumnRange, b: LineAndColumnRange) {
     if (comparePositions(b.start, a.end) >= 0) {
         return false;
@@ -100,17 +100,6 @@ export function doRangesOverlap(a: LineAndColumnRange, b: LineAndColumnRange) {
         return false;
     }
     return true;
-}
-
-export function convertRange(range?: LineAndColumnRange): Range {
-    if (!range) {
-        return Range.create(convertPosition(), convertPosition());
-    }
-    return Range.create(convertPosition(range.start), convertPosition(range.end));
-}
-
-export function convertPosition(position?: LineAndColumn): Position {
-    return !position ? Position.create(0, 0) : Position.create(position.line, position.character);
 }
 
 export function doesRangeContain(range: LineAndColumnRange, position: LineAndColumn) {
@@ -133,4 +122,15 @@ export function getEmptyRange(): LineAndColumnRange {
 export interface DocumentLineAndColumnRange {
     path: string;
     range: LineAndColumnRange;
+}
+
+export function convertRange(range?: LineAndColumnRange): lsp.Range {
+    if (!range) {
+        return lsp.Range.create(convertPosition(), convertPosition());
+    }
+    return lsp.Range.create(convertPosition(range.start), convertPosition(range.end));
+}
+
+export function convertPosition(position?: LineAndColumn): lsp.Position {
+    return !position ? lsp.Position.create(0, 0) : lsp.Position.create(position.line, position.character);
 }
