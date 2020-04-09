26fd8b1188641ca09a54c48a50588f10c455860a
Eric Traut <erictr@microsoft.com>
2020-04-09 14:52:58 -0700

    Revert "Changed text representation of functions to omit "Unused" parameter type annotations. This makes hover text and completion suggestion text more readable in code bases that are lacking type annotations."
    This reverts commit 4bccdb6614c28cbd2a7839bd3ac90c583937ecdf.

--- server/src/analyzer/typeEvaluator.ts
+++ server/src/analyzer/typeEvaluator.ts
@@ -11798,6 +11798,8 @@ export function createTypeEvaluator(importLookup: ImportLookup): TypeEvaluator {
     }
 
     function printFunctionParts(type: FunctionType, recursionCount = 0): [string[], string] {
+        // Avoid printing type types if none of the parameters have known types.
+        const printInputTypes = type.details.parameters.some((param) => param.hasDeclaredType);
         const paramTypeStrings = type.details.parameters.map((param, index) => {
             let paramString = '';
             if (param.category === ParameterCategory.VarArgList) {
@@ -11812,8 +11814,7 @@ export function createTypeEvaluator(importLookup: ImportLookup): TypeEvaluator {
 
             if (param.category === ParameterCategory.Simple) {
                 if (param.name) {
-                    // Avoid printing type types if parameter have unknown type.
-                    if (param.hasDeclaredType) {
+                    if (printInputTypes) {
                         const paramType = FunctionType.getEffectiveParameterType(type, index);
                         const paramTypeString =
                             recursionCount < maxTypeRecursionCount ? printType(paramType, recursionCount + 1) : '';
