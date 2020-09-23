284bc2d5cc8389e590e19d11cf78402980466b06
Eric Traut <erictr@microsoft.com>
2020-09-22 22:16:46 -0700

    Added test case for wildcard imports with dunder all.

---
+++ packages/pyright-internal/src/tests/fourslash/import.wildcard.fourslash.ts
@@ -0,0 +1,27 @@
+/// <reference path="fourslash.ts" />
+
+// @filename: testpkg/py.typed
+// @library: true
+////
+
+// @filename: testpkg/__init__.py
+// @library: true
+//// __all__ = ["submod"]
+//// def foo():
+////    return
+
+// @filename: testpkg/submod.py
+// @library: true
+//// def test_func():
+////     print("hi")
+
+// @filename: .src/test.py
+//// # pyright: reportWildcardImportFromLibrary=false
+//// from testpkg import *
+//// submod.test_func()
+//// [|/*marker*/foo|]()
+
+// @ts-ignore
+await helper.verifyDiagnostics({
+    marker: { category: 'error', message: `"foo" is not defined` },
+});
