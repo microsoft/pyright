0100b048984ceb2b5d72b20811fc10798e0f7234
cdce8p <30130371+cdce8p@users.noreply.github.com>
2020-11-16 03:06:04 +0100

    Improved test

--- packages/pyright-internal/src/tests/fourslash/completions.importDunderNames.fourslash.ts
+++ packages/pyright-internal/src/tests/fourslash/completions.importDunderNames.fourslash.ts
@@ -1,14 +1,18 @@
 /// <reference path="fourslash.ts" />
 
 // @filename: test.py
-//// import __future__[|/*marker1*/|]
+//// import _[|/*marker1*/|]
 //// import __pycache__[|/*marker2*/|]
 
 // @ts-ignore
-await helper.verifyCompletion('exact', 'markdown', {
+await helper.verifyCompletion('included', 'markdown', {
     marker1: {
         completions: [{ label: '__future__', kind: Consts.CompletionItemKind.Module }],
     },
+});
+
+// @ts-ignore
+await helper.verifyCompletion('exact', 'markdown', {
     marker2: {
         completions: [],
     },
