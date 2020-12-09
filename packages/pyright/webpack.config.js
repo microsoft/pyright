68e65420d22be60f01d5d5e7411d8b78b3592bf3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2020-12-09 12:37:21 -0800

    Fix infinite source mapping recursion, add TS source maps in production (#1258)

--- packages/pyright/webpack.config.js
+++ packages/pyright/webpack.config.js
@@ -10,71 +10,75 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { monorepoResourceNameMapper } = require('../../build/lib/webpack');
 
 const outPath = path.resolve(__dirname, 'dist');
 const typeshedFallback = path.resolve(__dirname, '..', 'pyright-internal', 'typeshed-fallback');
 
-/**@type {import('webpack').Configuration}*/
-module.exports = {
-    context: __dirname,
-    entry: {
-        pyright: './src/pyright.ts',
-        'pyright-langserver': './src/langserver.ts',
-    },
-    target: 'node',
-    output: {
-        filename: '[name].js',
-        path: outPath,
-        devtoolModuleFilenameTemplate: '../[resource-path]',
-    },
-    devtool: 'source-map',
-    stats: {
-        all: false,
-        errors: true,
-        warnings: true,
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
+/**@type {(env: any, argv: { mode: 'production' | 'development' | 'none' }) => import('webpack').Configuration}*/
+module.exports = (_, { mode }) => {
+    return {
+        context: __dirname,
+        entry: {
+            pyright: './src/pyright.ts',
+            'pyright-langserver': './src/langserver.ts',
+        },
+        target: 'node',
+        output: {
+            filename: '[name].js',
+            path: outPath,
+            devtoolModuleFilenameTemplate:
+                mode === 'development' ? '../[resource-path]' : monorepoResourceNameMapper('pyright'),
+        },
+        devtool: mode === 'development' ? 'source-map' : 'nosources-source-map',
+        stats: {
+            all: false,
+            errors: true,
+            warnings: true,
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+            plugins: [
+                new TsconfigPathsPlugin({
+                    extensions: ['.ts', '.js'],
+                }),
+            ],
+        },
+        externals: {
+            fsevents: 'commonjs2 fsevents',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: 'tsconfig.json',
+                    },
+                },
+            ],
+        },
         plugins: [
-            new TsconfigPathsPlugin({
-                extensions: ['.ts', '.js'],
-            }),
+            new CleanWebpackPlugin(),
+            new CopyPlugin({ patterns: [{ from: typeshedFallback, to: 'typeshed-fallback' }] }),
         ],
-    },
-    externals: {
-        fsevents: 'commonjs2 fsevents',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                options: {
-                    configFile: 'tsconfig.json',
-                },
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyPlugin({ patterns: [{ from: typeshedFallback, to: 'typeshed-fallback' }] }),
-    ],
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                defaultVendors: {
-                    name: 'vendor',
-                    test: /[\\/]node_modules[\\/]/,
-                    chunks: 'all',
-                    priority: -10,
-                },
-                pyright: {
-                    name: 'pyright-internal',
-                    chunks: 'all',
-                    test: /[\\/]pyright-internal[\\/]/,
-                    priority: -20,
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    defaultVendors: {
+                        name: 'vendor',
+                        test: /[\\/]node_modules[\\/]/,
+                        chunks: 'all',
+                        priority: -10,
+                    },
+                    pyright: {
+                        name: 'pyright-internal',
+                        chunks: 'all',
+                        test: /[\\/]pyright-internal[\\/]/,
+                        priority: -20,
+                    },
                 },
             },
         },
-    },
+    };
 };
--- packages/vscode-pyright/webpack.config.js
+++ packages/vscode-pyright/webpack.config.js
@@ -10,55 +10,59 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { monorepoResourceNameMapper } = require('../../build/lib/webpack');
 
 const outPath = path.resolve(__dirname, 'dist');
 const typeshedFallback = path.resolve(__dirname, '..', 'pyright-internal', 'typeshed-fallback');
 
-/**@type {import('webpack').Configuration}*/
-module.exports = {
-    context: __dirname,
-    entry: {
-        extension: './src/extension.ts',
-        server: './src/server.ts',
-    },
-    target: 'node',
-    output: {
-        filename: '[name].js',
-        path: outPath,
-        libraryTarget: 'commonjs2',
-        devtoolModuleFilenameTemplate: '../[resource-path]',
-    },
-    devtool: 'source-map',
-    stats: {
-        all: false,
-        errors: true,
-        warnings: true,
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-        plugins: [
-            new TsconfigPathsPlugin({
-                extensions: ['.ts', '.js'],
-            }),
-        ],
-    },
-    externals: {
-        vscode: 'commonjs vscode',
-        fsevents: 'commonjs2 fsevents',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                options: {
-                    configFile: 'tsconfig.json',
+/**@type {(env: any, argv: { mode: 'production' | 'development' | 'none' }) => import('webpack').Configuration}*/
+module.exports = (_, { mode }) => {
+    return {
+        context: __dirname,
+        entry: {
+            extension: './src/extension.ts',
+            server: './src/server.ts',
+        },
+        target: 'node',
+        output: {
+            filename: '[name].js',
+            path: outPath,
+            libraryTarget: 'commonjs2',
+            devtoolModuleFilenameTemplate:
+                mode === 'development' ? '../[resource-path]' : monorepoResourceNameMapper('vscode-pyright'),
+        },
+        devtool: mode === 'development' ? 'source-map' : 'nosources-source-map',
+        stats: {
+            all: false,
+            errors: true,
+            warnings: true,
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+            plugins: [
+                new TsconfigPathsPlugin({
+                    extensions: ['.ts', '.js'],
+                }),
+            ],
+        },
+        externals: {
+            vscode: 'commonjs vscode',
+            fsevents: 'commonjs2 fsevents',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: 'tsconfig.json',
+                    },
                 },
-            },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new CopyPlugin({ patterns: [{ from: typeshedFallback, to: 'typeshed-fallback' }] }),
         ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyPlugin({ patterns: [{ from: typeshedFallback, to: 'typeshed-fallback' }] }),
-    ],
+    };
 };
