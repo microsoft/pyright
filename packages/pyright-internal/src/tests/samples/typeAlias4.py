412a0ce363b182d07819d00347d8c9f92a5b4e00
Eric Traut <erictr@microsoft.com>
2021-03-01 14:12:21 -0700

    Changed the logic that determines whether a variable assignment is an implicit type alias definition. If there is an explicit type annotation (other than the use of the PEP 612 TypeAlias), it is no longer considered a type alias. This is consistent with the rules mypy uses.

--- packages/pyright-internal/src/tests/samples/typeAlias4.py
+++ packages/pyright-internal/src/tests/samples/typeAlias4.py
@@ -1,19 +1,23 @@
 # This sample tests the handling of the Python 3.9
 # TypeAlias feature as documented in PEP 613.
 
-from typing import TypeAlias as TA, Union
+from typing import Literal, Type, TypeAlias as TA, Union
 
 type1: TA = Union[int, str]
 
 type2: TA = "ClassA"
 
+
 class ClassA:
     pass
 
+
 not_a_type = "ClassA"
+
 
 def requires_string(a: str):
     pass
+
 
 requires_string(not_a_type)
 
@@ -42,3 +46,13 @@ my_type5: TA = True
 # This should generate an error because the expression
 # on the RHS evaluates to an object, not a class.
 my_type7: TA = list()
+
+# Verify that variables with declarations (other than explicit TypeAlias)
+# are not treated as a type alias.
+SimpleAlias = int
+ExplicitAlias: TA = int
+SimpleNonAlias: Type[int] = int
+
+t1: Literal["SimpleAlias"] = reveal_type(SimpleAlias)
+t2: Literal["ExplicitAlias"] = reveal_type(ExplicitAlias)
+t3: Literal["Type[int]"] = reveal_type(SimpleNonAlias)
