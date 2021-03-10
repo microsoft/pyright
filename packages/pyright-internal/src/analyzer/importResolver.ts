f1cfdd124732ea0e67a084cc997c6046b3d6da20
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2021-03-10 11:32:41 -0800

    Fix more symlink cases, add tryStat helper to clean up many try/catch stat calls

--- packages/pyright-internal/src/analyzer/importResolver.ts
+++ packages/pyright-internal/src/analyzer/importResolver.ts
@@ -31,6 +31,7 @@ import {
     resolvePaths,
     stripFileExtension,
     stripTrailingDirectorySeparator,
+    tryStat,
 } from '../common/pathUtils';
 import { equateStringsCaseInsensitive } from '../common/stringUtils';
 import * as StringUtils from '../common/stringUtils';
@@ -494,12 +495,7 @@ export class ImportResolver {
             if (!this.fileSystem.existsSync(path)) {
                 return false;
             }
-            try {
-                const stats = this.fileSystem.statSync(path);
-                return stats.isFile();
-            } catch {
-                return false;
-            }
+            return tryStat(this.fileSystem, path)?.isFile() ?? false;
         }
 
         const entries = this.readdirEntriesCached(splitPath[0]);
@@ -525,12 +521,7 @@ export class ImportResolver {
             if (!this.fileSystem.existsSync(path)) {
                 return false;
             }
-            try {
-                const stats = this.fileSystem.statSync(path);
-                return stats.isDirectory();
-            } catch {
-                return false;
-            }
+            return tryStat(this.fileSystem, path)?.isDirectory() ?? false;
         }
 
         const entries = this.readdirEntriesCached(splitPath[0]);
