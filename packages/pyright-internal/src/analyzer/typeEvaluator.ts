6d905e17e245e5846e74e4344199406a487211e9
Eric Traut <erictr@microsoft.com>
2021-05-01 23:27:31 -0700

    Removed PEP 563 (deferred type annotation) behavior as default for Python 3.10, reflecting a recent decision by the Python steering committee.

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -1336,7 +1336,6 @@ export function createTypeEvaluator(
     function isAnnotationEvaluationPostponed(fileInfo: AnalyzerFileInfo) {
         return (
             fileInfo.futureImports.get('annotations') !== undefined ||
-            fileInfo.executionEnvironment.pythonVersion >= PythonVersion.V3_10 ||
             fileInfo.isStubFile
         );
     }
