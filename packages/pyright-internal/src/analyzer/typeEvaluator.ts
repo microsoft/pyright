4d9f896dc789cf56e6271e362d29a4e03d52bd43
Eric Traut <erictr@microsoft.com>
2021-05-03 10:20:12 -0700

    Added back PEP 563 "on by default" for Python 3.11.

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -1334,7 +1334,11 @@ export function createTypeEvaluator(
     }
 
     function isAnnotationEvaluationPostponed(fileInfo: AnalyzerFileInfo) {
-        return fileInfo.futureImports.get('annotations') !== undefined || fileInfo.isStubFile;
+        return (
+            fileInfo.futureImports.get('annotations') !== undefined ||
+            fileInfo.executionEnvironment.pythonVersion >= PythonVersion.V3_11 ||
+            fileInfo.isStubFile
+        );
     }
 
     function getTypeOfAnnotation(
