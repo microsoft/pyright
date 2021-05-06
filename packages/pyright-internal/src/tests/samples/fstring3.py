df8b6b3c27ea4243ab8c1b472ecbbca157ab8bad
Eric Traut <erictr@microsoft.com>
2021-05-06 07:57:26 -0700

    Improved fstring3 unit test so it catches more potential errors.

--- packages/pyright-internal/src/tests/samples/fstring3.py
+++ packages/pyright-internal/src/tests/samples/fstring3.py
@@ -1,24 +1,26 @@
 # This sample tests f-strings where expressions contain
 # other string literals.
 
+# pyright: strict
+
 a1 = f"[{{name}}{'}' if True else ''}]"
 
 b1 = f"{'''hello'''}"
 
 c1 = f"""{"\""}"""
 
-hello = 3
-d1 = f"{ f'{hello}' }"
+hello1 = 3
+d1 = f"{ f'{hello1}' }"
 
-print(f"{'a' if 'b' != 'c' else 'd'}")
+print(f"{'a' if 'b' != d1 else 'd'}")
 
 
 a2 = fr"[{{name}}{'}' if True else ''}]"
 
 b2 = fr"{'''hello'''}"
 
-c2 = fr"""{"\""}"""
+c2 = rf"""{"\""}"""
 
-hello = 3
-d2 = fr"{ f'{hello}' }"
+hello2 = 3
+d2 = fr"{ rf'{hello2}' }"
 
