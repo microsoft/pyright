3053ff314cc65fc877aac6a2af0dad0f1c9d4900
Eric Traut <erictr@microsoft.com>
2021-06-02 21:26:14 -0700

    Closed a hole in type narrowing for "in" operator when used with TypedDict. It can eliminate types from a union only if the type is marked final.

--- packages/pyright-internal/src/tests/samples/typeNarrowing18.py
+++ packages/pyright-internal/src/tests/samples/typeNarrowing18.py
@@ -1,22 +1,30 @@
 # This sample tests type narrowing for TypedDict types based
 # on whether a key is in or not in the dict.
 
-from typing import Literal, TypedDict, Union
+from typing import Literal, TypedDict, Union, final
 
 
+@final
 class TD1(TypedDict):
     a: str
     b: int
 
 
+@final
 class TD2(TypedDict):
     a: int
     c: str
 
 
+@final
 class TD3(TypedDict, total=False):
     a: int
     d: str
+
+
+class TD4(TypedDict):
+    a: int
+    c: str
 
 
 def f1(p: Union[TD1, TD2]):
@@ -78,7 +86,14 @@ def f6(p: Union[TD1, TD2, TD3]):
 def f7(p: TD3):
     pass
 
+
 def f8(p: TD3):
     if "a" in p:
         f7(p)
 
+
+def f9(p: Union[TD1, TD4]):
+    if "b" in p:
+        tp1: Literal["TD1 | TD4"] = reveal_type(p)
+    else:
+        tp2: Literal["TD4"] = reveal_type(p)
