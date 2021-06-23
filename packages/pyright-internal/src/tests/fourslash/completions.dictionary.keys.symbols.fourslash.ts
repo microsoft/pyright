aa65071daec755330d49a429929b53d37bfa44c0
Bill Schnurr <bschnurr@microsoft.com>
2021-06-23 08:46:17 -0700

    singe file mode import fixes, dict key compleition fixes, fixed assert in documentSymbolCollector.ts

---
+++ packages/pyright-internal/src/tests/fourslash/completions.dictionary.keys.symbols.fourslash.ts
@@ -0,0 +1,53 @@
+/// <reference path="fourslash.ts" />
+
+// @filename: string_literals_with_symbols.py
+//// d = { "key-1" : 1 }
+//// d[[|"/*marker1*/"|]]
+
+// @filename: string_literals_with_symbols2.py
+//// d = { "key\"yo\"" : 1 }
+//// d[[|"/*marker2*/"|]]
+
+// @filename: string_literals_duplicates.py
+//// d = { "hello" : 1 }
+//// d["hello"] = 2
+////
+//// d[[|"/*marker3*/"|]]
+
+{
+    helper.openFiles(helper.getMarkers().map((m) => m.fileName));
+
+    // @ts-ignore
+    await helper.verifyCompletion('exact', 'markdown', {
+        marker1: {
+            completions: [
+                {
+                    label: '"key-1"',
+                    kind: Consts.CompletionItemKind.Constant,
+                    textEdit: { range: helper.getPositionRange('marker1'), newText: '"key-1"' },
+                    detail: 'Dictionary key',
+                },
+            ],
+        },
+        marker2: {
+            completions: [
+                {
+                    label: '"key\\"yo\\""',
+                    kind: Consts.CompletionItemKind.Constant,
+                    textEdit: { range: helper.getPositionRange('marker2'), newText: '"key\\"yo\\""' },
+                    detail: 'Dictionary key',
+                },
+            ],
+        },
+        marker3: {
+            completions: [
+                {
+                    label: '"hello"',
+                    kind: Consts.CompletionItemKind.Constant,
+                    textEdit: { range: helper.getPositionRange('marker3'), newText: '"hello"' },
+                    detail: 'Dictionary key',
+                },
+            ],
+        },
+    });
+}
