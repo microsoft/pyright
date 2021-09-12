ac342d8aa779ba217dfd848a8abd234de3a065bf
Eric Traut <erictr@microsoft.com>
2021-09-11 21:59:54 -0700

    Fixed bug in NewType test.

--- packages/pyright-internal/src/tests/samples/newType4.py
+++ packages/pyright-internal/src/tests/samples/newType4.py
@@ -1,5 +1,8 @@
 # This sample tests some error conditions for NewType usage.
 
+from typing import NewType
+
+
 A = NewType("A", Union[int, str])
 B = NewType("B", Literal[1])
 C = NewType("B", Sized)
