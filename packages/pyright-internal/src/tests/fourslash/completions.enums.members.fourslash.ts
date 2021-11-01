05633a620e553770a8fbbe982d9311f8cdc042c7
Eric Traut <eric@traut.com>
2021-11-01 14:46:14 -0700

    Added support in completion provide for enum members. They are now properly identified as such and prioritized higher in the completion list than other non-member symbols. (#2520)
    Co-authored-by: Eric Traut <erictr@microsoft.com>

---
+++ packages/pyright-internal/src/tests/fourslash/completions.enums.members.fourslash.ts
@@ -0,0 +1,44 @@
+/// <reference path="fourslash.ts" />
+
+// @filename: test.py
+//// from enum import Enum
+//// class Color(Enum):
+////     RED = 1
+////     GREEN = 2
+////     BLUE = 3
+////
+////     NotAMember: int = 3
+////
+////     @property
+////     def a_prop(self):
+////         pass
+////
+//// Color./*marker*/
+
+// @ts-ignore
+await helper.verifyCompletion('included', 'markdown', {
+    marker: {
+        completions: [
+            {
+                label: 'BLUE',
+                kind: Consts.CompletionItemKind.EnumMember,
+            },
+            {
+                label: 'GREEN',
+                kind: Consts.CompletionItemKind.EnumMember,
+            },
+            {
+                label: 'RED',
+                kind: Consts.CompletionItemKind.EnumMember,
+            },
+            {
+                label: 'a_prop',
+                kind: Consts.CompletionItemKind.Property,
+            },
+            {
+                label: 'NotAMember',
+                kind: Consts.CompletionItemKind.Variable,
+            },
+        ],
+    },
+});
