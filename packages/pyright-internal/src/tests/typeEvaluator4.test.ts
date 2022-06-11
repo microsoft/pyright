df23be59b907473f9e8f7f24f19b65be999500db
Eric Traut <erictr@microsoft.com>
2022-06-10 17:27:49 -0700

    Undid previous fix related to circular class dependency detection because it introduced a regression with sqlalchemy stubs.

--- packages/pyright-internal/src/tests/typeEvaluator4.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluator4.test.ts
@@ -1097,11 +1097,12 @@ test('Circular1', () => {
     TestUtils.validateResults(analysisResults, 0);
 });
 
-test('Circular2', () => {
-    const analysisResults = TestUtils.typeAnalyzeSampleFiles(['circular2.py']);
+// This test does not currently pass.
+// test('Circular2', () => {
+//     const analysisResults = TestUtils.typeAnalyzeSampleFiles(['circular2.py']);
 
-    TestUtils.validateResults(analysisResults, 0);
-});
+//     TestUtils.validateResults(analysisResults, 0);
+// });
 
 test('TryExcept1', () => {
     const analysisResults = TestUtils.typeAnalyzeSampleFiles(['tryExcept1.py']);
