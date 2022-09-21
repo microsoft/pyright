9fd1b94c87f3cc608be2c337241054312b30e715
Erik De Bonte <erikd@microsoft.com>
2022-09-21 08:34:43 -0700

    Fix argsRemainingCount calculation (#3975)

--- packages/pyright-internal/src/tests/typeEvaluator1.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluator1.test.ts
@@ -624,12 +624,12 @@ test('Call3', () => {
     // Analyze with Python 3.7 settings. This will generate more errors.
     configOptions.defaultPythonVersion = PythonVersion.V3_7;
     const analysisResults37 = TestUtils.typeAnalyzeSampleFiles(['call3.py'], configOptions);
-    TestUtils.validateResults(analysisResults37, 32);
+    TestUtils.validateResults(analysisResults37, 36);
 
     // Analyze with Python 3.8 settings.
     configOptions.defaultPythonVersion = PythonVersion.V3_8;
     const analysisResults38 = TestUtils.typeAnalyzeSampleFiles(['call3.py'], configOptions);
-    TestUtils.validateResults(analysisResults38, 18);
+    TestUtils.validateResults(analysisResults38, 20);
 });
 
 test('Call4', () => {
