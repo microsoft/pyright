d0cd3658a751f13f4dbc0aed9a54f9e155c0ad7c
Eric Traut <erictr@microsoft.com>
2022-10-31 08:36:04 -0700

    Fixed a bug in the type evaluator related to TypeVarTuple that caused it to incorrectly report a type incompatibility between `*Ts` and `*tuple[*Ts]`. These are the same type, so they should be compatible. This addresses https://github.com/microsoft/pyright/issues/4112.

--- packages/pyright-internal/src/tests/samples/variadicTypeVar5.py
+++ packages/pyright-internal/src/tests/samples/variadicTypeVar5.py
@@ -129,3 +129,10 @@ d5_2 = func4(callback4)
 
 d5_3 = func4(callback8)
 reveal_type(d5_3, expected_text="(int, str, complex) -> int")
+
+
+def func5(x: Callable[[Unpack[_Xs]], None], y: tuple[Unpack[_Xs]]):
+    pass
+
+def func6(x: Callable[[Unpack[_Xs]], None], y: tuple[Unpack[_Xs]]):
+    func5(x, y)
