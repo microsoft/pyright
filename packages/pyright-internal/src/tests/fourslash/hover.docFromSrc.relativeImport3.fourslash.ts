27f37983f89edf0103e0830fe394687fcc186eb6
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-12-05 16:25:40 -0800

    Add test with docstring in stub

--- packages/pyright-internal/src/tests/fourslash/hover.docFromSrc.relativeImport3.fourslash.ts
+++ packages/pyright-internal/src/tests/fourslash/hover.docFromSrc.relativeImport3.fourslash.ts
@@ -29,6 +29,7 @@
 //// # empty
 
 // @filename: typings/dj/db/models/__init__.pyi
+//// '''models doc string'''
 //// from .base import Model as Model
 
 // @filename: typings/dj/db/models/base.pyi
@@ -36,7 +37,7 @@
 ////     def clean_fields(self) -> None: ...
 
 // @filename: test.py
-//// from [|/*djmarker*/dj|].[|/*dbmarker*/db|] import models
+//// from [|/*djmarker*/dj|].[|/*dbmarker*/db|] import [|/*modelsmarker*/models|]
 ////
 //// class Person(models.Model):
 ////     pass
@@ -48,4 +49,5 @@ helper.verifyHover('markdown', {
     marker: '```python\n(method) clean_fields() -> None\n```\n---\nclean\\_fields docs',
     djmarker: '```python\n(module) dj\n```\n---\ndj doc string',
     dbmarker: '```python\n(module) db\n```\n---\ndb doc string',
+    modelsmarker: '```python\n(module) models\n```\n---\nmodels doc string',
 });
