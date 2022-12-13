3c2d1230cd0793e2a57e4b2613b5ae37d0615bfe
Eric Traut <erictr@microsoft.com>
2022-12-13 00:40:30 -0800

    Fixed a bug that led to a false positive when a `NoReturn` or `Never` type argument was used for a covariant type parameter. This addresses https://github.com/microsoft/pyright/issues/4309.

---
+++ packages/pyright-internal/src/tests/samples/never2.py
@@ -0,0 +1,23 @@
+# This sample validates that Never is treated as a bottom type for
+# covariant type arguments.
+
+from typing import Generic, Never, TypeVar
+
+U = TypeVar("U")
+
+T_co = TypeVar("T_co", covariant=True)
+class ClassA(Generic[T_co]):
+    pass
+
+def func1(x: U) -> ClassA[U]:
+    return ClassA[Never]()
+
+
+T = TypeVar("T")
+class ClassB(Generic[T]):
+    pass
+
+def func2(x: U) -> ClassB[U]:
+    # This should generate an error because T is invariant.
+    return ClassB[Never]()
+
