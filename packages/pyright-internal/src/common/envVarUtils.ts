5d3c85c0566c1a97f3ce2254f823d54a106f1914
PylanceBot <PylanceBot@microsoft.com>
2023-01-18 19:07:30 +0000

    pull-pylance-with-pyright-1.1.290

--- packages/pyright-internal/src/common/envVarUtils.ts
+++ packages/pyright-internal/src/common/envVarUtils.ts
@@ -1,3 +1,5 @@
+import * as os from 'os';
+
 /*
  * envVarUtils.ts
  * Copyright (c) Microsoft Corporation.
@@ -10,20 +12,23 @@
 // Ideally, VS Code would provide an API for doing this expansion, but
 // it doesn't. We'll handle the most common variables here as a convenience.
 export function expandPathVariables(rootPath: string, value: string): string {
-    const regexp = /\$\{(.*?)\}/g;
+    const regexp = /(\$\{.*?\}|~)/g;
     return value.replace(regexp, (match: string, name: string) => {
         const trimmedName = name.trim();
-        if (trimmedName === 'workspaceFolder') {
+        if (trimmedName === '${workspaceFolder}') {
             return rootPath;
         }
-        if (trimmedName === 'env:HOME' && process.env.HOME !== undefined) {
+        if (trimmedName === '${env:HOME}' && process.env.HOME !== undefined) {
             return process.env.HOME;
         }
-        if (trimmedName === 'env:USERNAME' && process.env.USERNAME !== undefined) {
+        if (trimmedName === '${env:USERNAME}' && process.env.USERNAME !== undefined) {
             return process.env.USERNAME;
         }
-        if (trimmedName === 'env:VIRTUAL_ENV' && process.env.VIRTUAL_ENV !== undefined) {
+        if (trimmedName === '${env:VIRTUAL_ENV}' && process.env.VIRTUAL_ENV !== undefined) {
             return process.env.VIRTUAL_ENV;
+        }
+        if (trimmedName === '~' && os.homedir) {
+            return os.homedir() || process.env.HOME || process.env.USERPROFILE || '~';
         }
         return match;
     });
