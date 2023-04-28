7a73d51b6b7ddf8908670cff3a716baf62140436
Erik De Bonte <erikd@microsoft.com>
2023-04-28 13:35:35 -0700

    Test errors

--- packages/pyright-internal/src/tests/samples/dataclass25.py
+++ packages/pyright-internal/src/tests/samples/dataclass25.py
@@ -23,20 +23,21 @@ def converter_with_default_for_first_param(s: str = "1") -> int:
 
 @dataclass
 class Foo:
-    field0: int = field(converter=converter_simple)                       # type: ignore
-    field1: int = field(converter=converter_with_param_before_args)       # type: ignore
-    field2: int = field(converter=converter_with_args)                    # type: ignore
-    field3: int = field(converter=converter_with_extra_defaulted_params)  # type: ignore
-    field4: int = field(converter=converter_with_default_for_first_param) # type: ignore
+    field0: int = field(converter=converter_simple)
+    field1: int = field(converter=converter_with_param_before_args)
+    field2: int = field(converter=converter_with_args)
+    field3: int = field(converter=converter_with_extra_defaulted_params)
+    field4: int = field(converter=converter_with_default_for_first_param)
 
 
 reveal_type(Foo.__init__, expected_text="(self: Foo, field0: str, field1: str, field2: str, field3: str, field4: str) -> None")
 
 
-# @overload
-# def overloaded_converter(s: float, secondParam: str = "foo") -> int: ...
+# This overload will be ignored because it has too many arguments.
+@overload
+def overloaded_converter(s: float, secondParam: str, /) -> int: ...
 
-# This overload will be ignored because it doesn't match the field type.
+# This overload will be ignored because its return type doesn't match the field type.
 @overload
 def overloaded_converter(s: float) -> str: ...
 
@@ -46,14 +47,35 @@ def overloaded_converter(s: str) -> int: ...
 @overload
 def overloaded_converter(s: list[str]) -> int: ...
 
-def overloaded_converter(s: float | str | list[str]) -> int | float | str:
+def overloaded_converter(s: float | str | list[str], *args: str) -> int | float | str:
     return 0
 
 
 @dataclass
 class Overloads:
-    field0: int = field(converter=overloaded_converter) # type: ignore
+    field0: int = field(converter=overloaded_converter)
 
 
 reveal_type(Overloads.__init__, expected_text="(self: Overloads, field0: str | list[str]) -> None")
 
+
+def wrong_return_type(s: str) -> str:
+    return s
+
+def wrong_number_of_params(x: str, x2: str, /) -> int:
+    return 1
+
+@overload
+def wrong_converter_overload(s: float) -> str: ...
+
+@overload
+def wrong_converter_overload(s: str) -> str: ...
+
+def wrong_converter_overload(s: float | str) -> int | str:
+    return 1
+
+@dataclass
+class Errors:
+    field0: int = field(converter=wrong_return_type)
+    field1: int = field(converter=wrong_number_of_params)
+    field2: int = field(converter=wrong_converter_overload)
