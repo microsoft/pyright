021c35bddbf495360a23f4b5e948f866fd750b2b
Rich Chiodo <rchiodo@users.noreply.github.com>
2023-04-28 11:11:52 -0700

    Add a test for edit mode

--- packages/pyright-internal/src/tests/workspaceEditUtils.test.ts
+++ packages/pyright-internal/src/tests/workspaceEditUtils.test.ts
@@ -9,11 +9,12 @@
 import * as assert from 'assert';
 import { TextDocumentEdit } from 'vscode-languageserver-types';
 
+import { CancellationToken } from 'vscode-languageserver';
 import { convertPathToUri } from '../common/pathUtils';
 import { applyWorkspaceEdit, generateWorkspaceEdit } from '../common/workspaceEditUtils';
 import { AnalyzerServiceExecutor } from '../languageService/analyzerServiceExecutor';
 import { TestLanguageService } from './harness/fourslash/testLanguageService';
-import { parseAndGetTestState, TestState } from './harness/fourslash/testState';
+import { TestState, parseAndGetTestState } from './harness/fourslash/testState';
 import { verifyWorkspaceEdit } from './harness/fourslash/workspaceEditTestUtils';
 
 test('test applyWorkspaceEdits changes', async () => {
@@ -44,6 +45,55 @@ test('test applyWorkspaceEdits changes', async () => {
 
     assert.strictEqual(fileChanged.size, 1);
     assert.strictEqual(cloned.test_program.getSourceFile(range.fileName)?.getFileContent(), 'Text Changed');
+});
+
+test('test edit mode for workspace', async () => {
+    const code = `
+// @filename: test.py
+//// [|/*marker*/|]
+            `;
+
+    const state = parseAndGetTestState(code).state;
+    state.workspace.service.enterEditMode();
+    const range = state.getRangeByMarkerName('marker')!;
+
+    const fileChanged = new Set<string>();
+    applyWorkspaceEdit(
+        state.workspace.service,
+        {
+            documentChanges: [
+                TextDocumentEdit.create(
+                    {
+                        uri: convertPathToUri(state.workspace.service.fs, range.fileName),
+                        version: null,
+                    },
+                    [
+                        {
+                            range: state.convertPositionRange(range),
+                            newText: 'import sys',
+                        },
+                    ]
+                ),
+            ],
+        },
+        fileChanged
+    );
+
+    assert.strictEqual(fileChanged.size, 1);
+    const newSourceFile = state.workspace.service.test_program.getSourceFile(range.fileName);
+    state.workspace.service.backgroundAnalysisProgram.analyzeFile(newSourceFile!.getFilePath(), CancellationToken.None);
+    assert.strictEqual(newSourceFile?.getFileContent(), 'import sys');
+    assert.strictEqual(newSourceFile.getImports().length, 2);
+
+    const edits = state.workspace.service.leaveEditMode();
+
+    // After leaving edit mode, we should be back to where we were.
+    const oldSourceFile = state.workspace.service.test_program.getSourceFile(range.fileName);
+    state.workspace.service.backgroundAnalysisProgram.analyzeFile(oldSourceFile!.getFilePath(), CancellationToken.None);
+    assert.strictEqual(oldSourceFile?.getFileContent(), '');
+    assert.strictEqual(oldSourceFile.getImports().length, 1);
+    assert.strictEqual(edits.length, 1);
+    assert.deepStrictEqual(edits[0].replacementText, 'import sys');
 });
 
 test('test applyWorkspaceEdits documentChanges', async () => {
