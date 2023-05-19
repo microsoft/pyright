ae791a88adfab8152412178d46dd22a51fe81a45
Erik De Bonte <erikd@microsoft.com>
2023-05-19 11:37:36 -0700

    TextRange.combine should not modify input

---
+++ packages/pyright-internal/src/tests/textRange.test.ts
@@ -0,0 +1,26 @@
+/*
+ * textRange.test.ts
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+import * as assert from 'assert';
+
+import { TextRange } from '../common/textRange';
+
+test('textRange combine', () => {
+    const range1 = TextRange.create(10, 2);
+    const range2 = TextRange.create(12, 2);
+
+    const combined = TextRange.combine([range1, range2]);
+
+    assert.ok(combined);
+    assert.equal(combined.start, 10);
+    assert.equal(combined.length, 4);
+
+    // Ensure input ranges are unchanged
+    assert.equal(range1.start, 10);
+    assert.equal(range1.length, 2);
+    assert.equal(range2.start, 12);
+    assert.equal(range2.length, 2);
+});
