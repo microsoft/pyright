2cd022020762e90f27af7fced914e5d2e4eff6c8
Eric Traut <eric@traut.com>
2023-06-15 20:48:54 -0700

    Fixed several subtle bugs in the overload matching algorithm in the case where Any or Unknown arguments are present. (#5316)
    Co-authored-by: Eric Traut <erictr@microsoft.com>

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -8347,6 +8347,17 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
             return matches;
         }
 
+        // If the relevance of some matches differs, filter out the ones that
+        // are lower relevance. This favors *args parameters in cases where
+        // a *args argument is used.
+        if (matches[0].matchResults.relevance !== matches[matches.length - 1].matchResults.relevance) {
+            matches = matches.filter((m) => m.matchResults.relevance === matches[0].matchResults.relevance);
+
+            if (matches.length < 2) {
+                return matches;
+            }
+        }
+
         // If all of the return types match, select the first one.
         if (
             areTypesSame(
@@ -8362,6 +8373,7 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
             return matches;
         }
 
+        let foundAmbiguousAnyArg = false;
         for (let i = 0; i < firstArgResults.length; i++) {
             // If the arg is Any or Unknown, see if the corresponding
             // parameter types differ in any way.
@@ -8372,9 +8384,18 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
                         : UnknownType.create()
                 );
                 if (!areTypesSame(paramTypes, { treatAnySameAsUnknown: true })) {
-                    return matches;
+                    foundAmbiguousAnyArg = true;
                 }
             }
+        }
+
+        // If the first overload has a different number of effective arguments
+        // than latter overloads, don't filter any of them. This typically means
+        // that one of the arguments is an unpacked iterator, and it maps to
+        // an indeterminate number of parameters, which means that the overload
+        // selection is ambiguous.
+        if (foundAmbiguousAnyArg || matches.some((match) => match.argResults.length !== firstArgResults.length)) {
+            return matches;
         }
 
         return [matches[0]];
