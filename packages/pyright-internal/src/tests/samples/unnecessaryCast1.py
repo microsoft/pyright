0ad4a6298bf0b6e695d1c1587a1e2b05a06ff651
Eric Traut <erictr@microsoft.com>
2023-06-18 11:58:44 -0700

    Improved `reportUnnecessaryCast` so it works with types other than class instances. This addresses https://github.com/microsoft/pyright/issues/5333.

--- packages/pyright-internal/src/tests/samples/unnecessaryCast1.py
+++ packages/pyright-internal/src/tests/samples/unnecessaryCast1.py
@@ -1,13 +1,54 @@
 # This sample tests the type checker's reportUnnecessaryCast feature.
 
-from typing import cast, Union
+from typing import Never, NoReturn, TypeVar, cast
 
 
-def foo(a: int):
+def func1(a: int):
     # This should generate an error if
     # reportUnnecessaryCast is enabled.
-    b = cast(int, a)
+    v1 = cast(int, a)
 
 
-c: Union[int, str] = "hello"
-d = cast(int, c)
+def func2(a: int | str):
+    v1 = cast(int, a)
+
+    b: str = "hello"
+    v2 = cast(int, b)
+
+
+def func3(a: int | None):
+    v1 = cast(int, a)
+
+    # This should generate an error if
+    # reportUnnecessaryCast is enabled.
+    v2 = cast(int | None, a)
+
+
+T = TypeVar("T")
+
+
+def func4(a: list[T]) -> list[T]:
+    # This should generate an error if
+    # reportUnnecessaryCast is enabled.
+    v1 = cast(list[T], a)
+
+    return a
+
+
+def func5(a: Never):
+    # This should generate an error if
+    # reportUnnecessaryCast is enabled.
+    v1 = cast(NoReturn, a)
+
+
+def func6(a: type[int], b: int):
+    v1 = cast(int, a)
+    v2 = cast(type[int], b)
+
+    # This should generate an error if
+    # reportUnnecessaryCast is enabled.
+    v3 = cast(type[int], a)
+
+    # This should generate an error if
+    # reportUnnecessaryCast is enabled.
+    v4 = cast(int, b)
