c8a25b2d6141a23907e3c6c0f188ec45122ec421
Eric Traut <erictr@microsoft.com>
2023-06-22 21:14:49 -0700

    Revert "Added mitigation for reported assertion failure (and crash). This temporarily addresses https://github.com/microsoft/pyright/issues/5355."
    This reverts commit 7b242d5e5ee70504cc43e4710d5f36b5db569b77.

--- packages/pyright-internal/src/analyzer/codeFlowEngine.ts
+++ packages/pyright-internal/src/analyzer/codeFlowEngine.ts
@@ -268,15 +268,8 @@ export function getCodeFlowEngine(
                 evaluationCount: number
             ) {
                 const cachedEntry = flowNodeTypeCache.cache.get(flowNode.id);
-                if (cachedEntry === undefined) {
-                    fail('setIncompleteSubtype can be called only on a valid cache entry');
-                }
-
-                if (!isIncompleteType(cachedEntry)) {
-                    // This condition should rarely happen, but it's possible in the case where we
-                    // have a nested loop and getTypeFromLoopFlowNode is called recursively when
-                    // the first antecedent is already marked as pending.
-                    return;
+                if (cachedEntry === undefined || !isIncompleteType(cachedEntry)) {
+                    fail('setIncompleteSubtype can be called only on a valid incomplete cache entry');
                 }
 
                 const incompleteEntries = cachedEntry.incompleteSubtypes;
