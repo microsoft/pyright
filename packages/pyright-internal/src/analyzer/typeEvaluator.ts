f9796dd5cd6806f6e7e2b0efd1a27786521b35f4
Eric Traut <eric@traut.com>
2023-07-12 00:22:35 +0200

    Fixed bug that led to a confusing hover type for the return result of an async generator function. This addresses #5411. (#5468)
    Co-authored-by: Eric Traut <erictr@microsoft.com>

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -16919,6 +16919,7 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
 
         // Note that the inferred type, once lazily computed, needs to wrap the
         // resulting type in an awaitable.
+        functionType.details.flags |= FunctionTypeFlags.WrapReturnTypeInAwait;
         awaitableFunctionType.details.flags |= FunctionTypeFlags.WrapReturnTypeInAwait;
 
         return awaitableFunctionType;
