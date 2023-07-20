98fcba13dd0dda843f7c9b35d353947a503fe4e4
Eric Traut <eric@traut.com>
2023-07-19 20:28:55 -0700

    Added support for the `X is C` and `X is not C` type guard pattern (where `C` is a class). This addresses #5490. (#5544)
    Co-authored-by: Eric Traut <erictr@microsoft.com>

---
+++ packages/pyright-internal/src/tests/samples/typeNarrowingIsClass1.py
@@ -0,0 +1,58 @@
+# This sample tests type narrowing for conditional
+# statements of the form X is <class> or X is not <class>.
+
+from typing import Any, TypeVar, final
+
+
+@final
+class A:
+    ...
+
+
+@final
+class B:
+    ...
+
+
+class C:
+    ...
+
+
+def func1(x: type[A] | type[B] | None | int):
+    if x is A:
+        reveal_type(x, expected_text="type[A]")
+    else:
+        reveal_type(x, expected_text="type[B] | int | None")
+
+
+def func2(x: type[A] | type[B] | None | int, y: type[A]):
+    if x is not y:
+        reveal_type(x, expected_text="type[B] | int | None")
+    else:
+        reveal_type(x, expected_text="type[A]")
+
+
+def func3(x: type[A] | type[B] | Any):
+    if x is A:
+        reveal_type(x, expected_text="type[A] | Any")
+    else:
+        reveal_type(x, expected_text="type[B] | Any")
+
+
+def func4(x: type[A] | type[B] | type[C]):
+    if x is C:
+        reveal_type(x, expected_text="type[C]")
+    else:
+        reveal_type(x, expected_text="type[A] | type[B] | type[C]")
+
+
+T = TypeVar("T")
+
+
+def func5(x: type[A] | type[B] | type[T]) -> type[A] | type[B] | type[T]:
+    if x is A:
+        reveal_type(x, expected_text="type[A] | type[T@func5]")
+    else:
+        reveal_type(x, expected_text="type[B] | type[T@func5]")
+
+    return x
