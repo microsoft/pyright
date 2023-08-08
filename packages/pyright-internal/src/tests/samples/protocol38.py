e353e9b493f88195043350e6bca4c8f163f202f4
Eric Traut <erictr@microsoft.com>
2023-08-07 18:34:30 -0700

    Fixed bug that led to a false positive error in certain cases when using bidirectional type inference when the expected type includes a protocol that is specialized with a literal type argument. This addresses https://github.com/microsoft/pyright/issues/5636.

--- packages/pyright-internal/src/tests/samples/protocol38.py
+++ packages/pyright-internal/src/tests/samples/protocol38.py
@@ -1,7 +1,7 @@
 # This sample tests the case where a protocol is specialized with
 # a literal type.
 
-from typing import Literal, Protocol, Self
+from typing import Any, Literal, Protocol, Self, TypeVar
 
 
 class Negatable(Protocol):
@@ -18,3 +18,15 @@ func1(0)
 
 def func2(val: Literal[0, 1]):
     func1(val)
+
+
+T = TypeVar("T", covariant=True)
+
+
+class SupportsGetItem(Protocol[T]):
+    def __getitem__(self, __k: int) -> T:
+        ...
+
+
+def func3(a: tuple[Any, ...]):
+    x: SupportsGetItem[Literal["a"]] = a
