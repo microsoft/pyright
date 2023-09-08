6e1dce3e1e14ae196537377ea55d2eadd9b9d9c6
Eric Traut <erictr@microsoft.com>
2023-09-08 10:35:32 -0700

    Fixed a recent regression that caused the pythonPath received from the client to be corrupted resulting in import resolution errors.

--- packages/pyright-internal/src/common/pathUtils.ts
+++ packages/pyright-internal/src/common/pathUtils.ts
@@ -536,7 +536,7 @@ export function stripFileExtension(fileName: string, multiDotExtension = false) 
 }
 
 export function realCasePath(pathString: string, fileSystem: ReadOnlyFileSystem): string {
-    return normalizePath(fileSystem.realCasePath(pathString));
+    return normalizePath(pathString);
 }
 
 export function normalizePath(pathString: string): string {
