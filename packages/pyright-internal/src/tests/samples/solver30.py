bd801c57f00317a0484c7198b5b33fc6936e2eb2
Eric Traut <erictr@microsoft.com>
2023-10-08 16:24:02 -0700

    Fixed a bug that led to incorrect type evaluation in a nested set of call expressions and a lambda. This addresses #6042.

---
+++ packages/pyright-internal/src/tests/samples/solver30.py
@@ -0,0 +1,37 @@
+# This sample tests the case where a deeply nested set of calls requires
+# the use of bidirectional type inference to evaluate the type of a lambda.
+
+from typing import Any, Callable, Iterable, Iterator, TypeVar
+
+X = TypeVar("X")
+Y = TypeVar("Y")
+Z = TypeVar("Z")
+
+
+class Item:
+    foo: bool
+
+
+items = [Item()]
+
+
+def func1(a: Iterable[X]) -> X:
+    ...
+
+
+def func2(a: Iterable[Y]) -> Iterable[Y]:
+    ...
+
+
+class func3(Iterator[Z]):
+    def __init__(self, a: Callable[[Z], Any], b: Iterable[Z]) -> None:
+        ...
+
+
+def func4(a: Callable[[Z], Any], b: Iterable[Z]) -> Iterator[Z]:
+    ...
+
+
+func1(func2(func3(lambda x: reveal_type(x.foo, expected_text="bool"), items)))
+
+func1(func2(func4(lambda x: reveal_type(x.foo, expected_text="bool"), items)))
