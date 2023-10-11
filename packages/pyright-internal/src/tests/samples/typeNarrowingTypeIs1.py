abc72c281c35bd2ec69d72107e94cc969ab058b9
Eric Traut <eric@traut.com>
2023-10-11 12:33:10 -0700

    Fixed regression in `type(x) is y` type narrowing logic when `y` is of type `type[Self]`. This addresses #6118. (#6125)
    Co-authored-by: Eric Traut <erictr@microsoft.com>

--- packages/pyright-internal/src/tests/samples/typeNarrowingTypeIs1.py
+++ packages/pyright-internal/src/tests/samples/typeNarrowingTypeIs1.py
@@ -113,8 +113,17 @@ def func8(a: _TC, b: _TC) -> _TC:
 
 
 class F:
-    def method1(self, other: object):
-        if type(self) == type(other):
+    def method1(self, v: object):
+        if type(self) == type(v):
             reveal_type(self, expected_text="Self@F")
         else:
             reveal_type(self, expected_text="Self@F")
+
+
+class G(str):
+    @classmethod
+    def method1(cls, v: str):
+        if type(v) is cls:
+            reveal_type(v, expected_text="G")
+        else:
+            reveal_type(v, expected_text="str")
