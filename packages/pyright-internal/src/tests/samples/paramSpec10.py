d42d3c0ce9eccb8a89a766bf61fafa7457329e2f
Eric Traut <eric@traut.com>
2023-10-14 10:22:25 -0700

    Fixed a bug in ParamSpec type matching that resulted in an incorrect type evaluation under certain circumstances. This addresses #6141.

--- packages/pyright-internal/src/tests/samples/paramSpec10.py
+++ packages/pyright-internal/src/tests/samples/paramSpec10.py
@@ -54,4 +54,4 @@ res3 = test_3(testClass, 42)
 reveal_type(res3, expected_text="str")
 
 res4: Callable[[MyClass, int], str] = with_lock(test_3)
-reveal_type(res4, expected_text="(MyClass, int) -> str")
+reveal_type(res4, expected_text="(MyClass, param1: int) -> str")
