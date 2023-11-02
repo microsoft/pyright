e8888660b4fe4be17d1670e302f7697e066227ea
Eric Traut <eric@traut.com>
2023-11-02 10:45:14 -0700

    Updated heuristics for constructor evaluation to accommodate a wider range of types returned by the `__new__` method. Previously, if the `__new__` return type was anything other than a class instance, the heuristics assumed it wasn't intended and assumed that `__new__` returned an instance of its class, as is usually the case. This addresses #6303.

--- packages/pyright-internal/src/tests/samples/constructor7.py
+++ packages/pyright-internal/src/tests/samples/constructor7.py
@@ -2,10 +2,30 @@
 # a type that differs from the class that contains it.
 
 
-class HelloWorld:
+from typing import Callable, ParamSpec, TypeVar
+
+
+class ClassA:
     def __new__(cls) -> str:
         return "Hello World"
 
 
-v1 = HelloWorld()
+v1 = ClassA()
 reveal_type(v1, expected_text="str")
+
+
+_P = ParamSpec("_P")
+_R = TypeVar("_R")
+
+
+def func1(a: int) -> int:
+    return a + 1
+
+
+class ClassB:
+    def __new__(cls, func: Callable[_P, _R]) -> Callable[_P, _R]:
+        return func
+
+
+v2 = ClassB(func1)
+reveal_type(v2, expected_text="(a: int) -> int")
