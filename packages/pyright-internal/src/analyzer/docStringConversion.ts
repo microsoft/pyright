1d18d4461ff7a15d503b570234a43e02af905a44
Bill <bschnurr@microsoft.com>
2023-11-02 12:47:11 -0700

    Add support non english works in docstring arguments

--- packages/pyright-internal/src/analyzer/docStringConversion.ts
+++ packages/pyright-internal/src/analyzer/docStringConversion.ts
@@ -589,7 +589,7 @@ class DocStringConverter {
 
         // catch-all for styles except reST
         const hasArguments =
-            !line?.endsWith(':') && !line?.endsWith('::') && !!line.match(/^\s*.*?\w+(\s*\(.*?\))*\s*:\s*\w+/g);
+            !line?.endsWith(':') && !line?.endsWith('::') && !!line.match(/^\s*.*?\S+(\s*\(.*?\))*\s*:\s*\S+/gu);
 
         // reSt params. Attempt to put directives lines into their own paragraphs.
         const restDirective = DirectivesExtraNewlineRegExp.test(line); //line.match(/^\s*:param/);
