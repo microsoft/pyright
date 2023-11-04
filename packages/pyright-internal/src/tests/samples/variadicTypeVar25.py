9511aabf90b3dda2ee0b21ae99403a5f9af35075
Eric Traut <eric@traut.com>
2023-11-04 00:39:28 -0700

    Fixed a bug that resulted in incorrect type evaluation when using a `TypeVarTuple` within a nested `Callable` type (i.e. a `Callable` that takes a `Callable` as a parameter). This addresses #6316. (#6322)

---
+++ packages/pyright-internal/src/tests/samples/variadicTypeVar25.py
@@ -0,0 +1,17 @@
+# This sample tests the case where a TypeVarTuple is used in a
+# nested callable type.
+
+from typing import Callable, TypeVarTuple
+
+Ts = TypeVarTuple("Ts")
+
+
+def func1(g: Callable[[Callable[[*Ts], None]], None]) -> tuple[*Ts]:
+    ...
+
+
+def func2(cb: Callable[[bytes, int], None]) -> None:
+    ...
+
+
+reveal_type(func1(func2), expected_text="tuple[bytes, int]")
