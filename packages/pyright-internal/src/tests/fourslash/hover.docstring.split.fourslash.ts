eeb5f11b76ba6ac4fff47b1a2980d781621e891a
Bill Schnurr <bschnurr@microsoft.com>
2023-11-14 17:18:24 -0800

    Add support non-English words in docstring arguments (#6307)
    * Add support non english works in docstring arguments
    
    * Relax argument regex to handle non english words but also be less strict with invalid types  in the google doc style with invalid types.
    
    ---------
    
    Co-authored-by: Eric Traut <eric@traut.com>

--- packages/pyright-internal/src/tests/fourslash/hover.docstring.split.fourslash.ts
+++ packages/pyright-internal/src/tests/fourslash/hover.docstring.split.fourslash.ts
@@ -13,12 +13,23 @@
 ////     '''This docstring ''' f'''is split.'''
 ////     pass
 ////
+//// def func4(a:int, b:int, c:int):
+////     """
+////     Args:
+////         a (int): description
+////         b (int|bool): 한국어
+////         c (int): description
+////     """
+////
 //// [|/*marker1*/func|]()
 //// [|/*marker2*/func2|]()
 //// [|/*marker3*/func3|]()
+//// [|/*marker4*/func4|]()
 
 helper.verifyHover('markdown', {
     marker1: '```python\n(function) def func() -> None\n```\n---\nThis docstring is split.',
     marker2: '```python\n(function) def func2() -> None\n```',
     marker3: '```python\n(function) def func3() -> None\n```',
+    marker4:
+        '```python\n(function) def func4(a: int, b: int, c: int) -> None\n```\n---\nArgs:  \n&nbsp;&nbsp;&nbsp;&nbsp;a (int): description  \n&nbsp;&nbsp;&nbsp;&nbsp;b (int|bool): 한국어  \n&nbsp;&nbsp;&nbsp;&nbsp;c (int): description',
 });
