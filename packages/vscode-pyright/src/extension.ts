8c9075513a243f85c223bf495f064dd75fda3fcf
rchiodo <rchiodo@microsoft.com>
2023-11-22 15:20:27 -0800

    Fix formatting

--- packages/vscode-pyright/src/extension.ts
+++ packages/vscode-pyright/src/extension.ts
@@ -71,9 +71,7 @@ export async function activate(context: ExtensionContext) {
 
     const bundlePath = context.asAbsolutePath(path.join('dist', 'server.js'));
     const runOptions = { execArgv: [`--max-old-space-size=${defaultHeapSize}`] };
-    const debugOptions = {
-        execArgv: ['--nolazy', '--inspect=6600', `--max-old-space-size=${defaultHeapSize}`],
-    };
+    const debugOptions = { execArgv: ['--nolazy', '--inspect=6600', `--max-old-space-size=${defaultHeapSize}`] };
 
     // If the extension is launched in debug mode, then the debug server options are used.
     const serverOptions: ServerOptions = {
