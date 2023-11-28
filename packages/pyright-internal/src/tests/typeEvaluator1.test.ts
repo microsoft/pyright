5b3523ff53227682d7a46351e07378932e4b87b5
Eric Traut <eric@traut.com>
2023-11-27 17:51:31 -0800

    Added support for parameter type inference for lambdas whose parameters include default arguments. This is analogous to the logic used for unannotated function parameters with default arguments. This addresses #6558. (#6563)

--- packages/pyright-internal/src/tests/typeEvaluator1.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluator1.test.ts
@@ -741,6 +741,12 @@ test('Lambda13', () => {
     TestUtils.validateResults(analysisResults, 0);
 });
 
+test('Lambda14', () => {
+    const analysisResults = TestUtils.typeAnalyzeSampleFiles(['lambda14.py']);
+
+    TestUtils.validateResults(analysisResults, 0);
+});
+
 test('Call1', () => {
     const analysisResults = TestUtils.typeAnalyzeSampleFiles(['call1.py']);
 
