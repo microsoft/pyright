16778e6cdd84f34892e0d1d33e489359dd384976
Eric Traut <eric@traut.com>
2024-01-09 12:27:45 -0800

    Fixed a broken internal assumption that was violated by recent typeshed change.

--- packages/pyright-internal/src/tests/samples/typeNarrowingIsEllipsis1.py
+++ packages/pyright-internal/src/tests/samples/typeNarrowingIsEllipsis1.py
@@ -13,12 +13,12 @@ def func1(val: int | ellipsis):
     if val is not ...:
         reveal_type(val, expected_text="int")
     else:
-        reveal_type(val, expected_text="ellipsis")
+        reveal_type(val, expected_text="EllipsisType")
 
 
 def func2(val: _T):
     if val is ...:
-        reveal_type(val, expected_text="ellipsis*")
+        reveal_type(val, expected_text="EllipsisType*")
     else:
         reveal_type(val, expected_text="str*")
 
@@ -27,11 +27,11 @@ def func3(val: int | types.EllipsisType):
     if val != ...:
         reveal_type(val, expected_text="int")
     else:
-        reveal_type(val, expected_text="ellipsis")
+        reveal_type(val, expected_text="EllipsisType")
 
 
 def func4(val: int | ellipsis):
     if not val == ...:
         reveal_type(val, expected_text="int")
     else:
-        reveal_type(val, expected_text="ellipsis")
+        reveal_type(val, expected_text="EllipsisType")
