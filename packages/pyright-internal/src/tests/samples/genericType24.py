8eb00c62e1abd78288b7b05b55b5f173db8cbbbc
Eric Traut <eric@traut.com>
2024-01-09 14:29:06 -0800

    Changed the way pyright identifies an "unimplemented protocol method" within a stub file. It now looks at whether the method is decorated with `@abstractmethod`. Previously, it assumed all such methods were potentially implemented. This addresses #6946.

--- packages/pyright-internal/src/tests/samples/genericType24.py
+++ packages/pyright-internal/src/tests/samples/genericType24.py
@@ -1,13 +1,14 @@
 # This sample tests a case where a default argument in a parent class
 # needs to be specialized in the context of a child class.
 
-from typing import Generic, Iterable, TypeVar
+from typing import Generic, Iterable, Iterator, TypeVar
 
 T = TypeVar("T")
 
 
 class IterableProxy(Iterable[T]):
-    ...
+    def __iter__(self) -> Iterator[T]:
+        ...
 
 
 class Parent(Generic[T]):
