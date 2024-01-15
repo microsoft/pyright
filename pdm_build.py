a78c1641b3ef2165ca559f6c73e6ecb8c34b6335
detachhead <detachhead@users.noreply.github.com>
2024-01-15 17:24:30 +1000

    fix pyright errors in `pdm_build.py` and remove unused variables

--- pdm_build.py
+++ pdm_build.py
@@ -6,9 +6,7 @@ from shutil import copytree
 from nodejs import npm
 
 if not Path("node_modules").exists():
-    npm.run(["ci"], check=True)
-npm.run(["run", "build:cli:dev"], check=True)
+    _ = npm.run(["ci"], check=True)
+_ = npm.run(["run", "build:cli:dev"], check=True)
 
-pyright_npm_package_dir = Path("packages/pyright")
-pyright_pypi_package_dir = Path("basedpyright")
 copytree("packages/pyright/dist", "basedpyright/dist", dirs_exist_ok=True)
