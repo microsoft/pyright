438920a6f07165d39680ddc0153f8dd5851ad4b5
Eric Traut <eric@traut.com>
2024-01-16 18:46:16 -0800

    Fixed a bug that resulted in an incorrect type evaluation when a generator uses an `await` operator within the left-most `for`. This shouldn't result in an `AsyncGenerator` despite what the Python documentation indicates. This addresses #6999.

--- packages/pyright-internal/src/tests/samples/generator4.py
+++ packages/pyright-internal/src/tests/samples/generator4.py
@@ -1,6 +1,8 @@
 # This sample tests various type checking operations relating to
 # async generator functions where the return type is inferred.
 
+from typing import AsyncGenerator, Awaitable, Generator, Iterator
+
 
 async def g1():
     yield 1
@@ -10,9 +12,6 @@ async def g1():
 async def g2():
     async for v in g1():
         yield v
-
-
-from typing import AsyncGenerator, Generator
 
 
 def g1_explicit1() -> Generator[int, None, None]:
@@ -31,3 +30,42 @@ async def g2_explicit():
 
     async for v in g1_explicit2():
         yield v
+
+
+async def g3(xs: Awaitable[list[int]]) -> list[int]:
+    return [x for x in await xs]
+
+
+async def g4(xs: list[Awaitable[int]]) -> list[int]:
+    return [await x for x in xs]
+
+
+class SomeIterable:
+    def __init__(self):
+        self.x = 1
+
+    def __iter__(self) -> Iterator[int]:
+        yield self.x
+
+
+async def func1() -> SomeIterable:
+    return SomeIterable()
+
+
+def func2() -> Iterator[int]:
+    yield 2
+
+
+def g5() -> None:
+    val = (y for y in func2())
+    reveal_type(val, expected_text="Generator[int, None, None]")
+
+
+async def g6() -> None:
+    val = (x + y for y in func2() for x in await func1())
+    reveal_type(val, expected_text="AsyncGenerator[int, None]")
+
+
+async def g7() -> None:
+    val = (x + y for y in await func1() for x in func2())
+    reveal_type(val, expected_text="Generator[int, None, None]")
