4b29f5148b69c103c32347158cadde3742d4b0f1
rchiodo <rchiodo@microsoft.com>
2024-01-18 10:42:08 -0800

    Remove unneeded code

--- packages/pyright-internal/src/common/uri/uriUtils.ts
+++ packages/pyright-internal/src/common/uri/uriUtils.ts
@@ -13,7 +13,6 @@ import { FileSystem, ReadOnlyFileSystem, Stats, TempFile } from '../fileSystem';
 import {
     getRegexEscapedSeparator,
     isDirectoryWildcardPatternPresent,
-    resolvePaths,
     stripTrailingDirectorySeparator,
 } from '../pathUtils';
 import { Uri } from './uri';
@@ -428,8 +427,4 @@ export function getRootUri(isCaseSensitive: boolean): Uri | undefined {
 export function encodeUri(fs: ReadOnlyFileSystem, uri: Uri): string {
     // Convert to a URI string that the LSP client understands (mapped files are only local to the server).
     return fs.getOriginalUri(uri).toString();
-}
-
-export function resolveFilePath(root: Uri, filePath: string) {
-    return Uri.file(resolvePaths(root.getFilePath(), filePath));
 }
