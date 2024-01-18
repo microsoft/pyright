36ff7e299d95409736f73cd8fe382d625b0f890b
Eric Traut <eric@traut.com>
2024-01-18 10:34:56 -0800

    Fixed another typeshed enum I missed.

--- packages/pyright-internal/typeshed-fallback/stdlib/ssl.pyi
+++ packages/pyright-internal/typeshed-fallback/stdlib/ssl.pyi
@@ -314,8 +314,8 @@ class _ASN1Object(_ASN1ObjectBase):
     def fromname(cls, name: str) -> Self: ...
 
 class Purpose(_ASN1Object, enum.Enum):
-    SERVER_AUTH: _ASN1Object
-    CLIENT_AUTH: _ASN1Object
+    SERVER_AUTH = ...
+    CLIENT_AUTH = ...
 
 class SSLSocket(socket.socket):
     context: SSLContext
