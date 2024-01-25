9a87f6d02fef7cc3abbed22c9e8dede58acba455
DetachHead <detachhead@users.noreply.github.com>
2024-01-25 15:56:24 +1000

    rename "informations" to "notes" in the diagnostic counts because "informations" looks weird

--- packages/pyright-internal/src/pyright.ts
+++ packages/pyright-internal/src/pyright.ts
@@ -930,7 +930,7 @@ function reportDiagnosticsAsText(
     console.info(
         `${errorCount.toString()} ${errorCount === 1 ? 'error' : 'errors'}, ` +
             `${warningCount.toString()} ${warningCount === 1 ? 'warning' : 'warnings'}, ` +
-            `${informationCount.toString()} ${informationCount === 1 ? 'information' : 'informations'} `
+            `${informationCount.toString()} ${informationCount === 1 ? 'note' : 'notes'} `
     );
 
     return {
