fe94d855f9313204b6f7c0992e319c560d10c67c
DetachHead <detachhead@users.noreply.github.com>
2024-02-06 10:40:13 +1000

    remove `Any`s from build and version scripts

--- get_version.py
+++ get_version.py
@@ -2,7 +2,14 @@ from __future__ import annotations
 
 from json import loads
 from pathlib import Path
+from typing import TypedDict, cast
+
+
+class LernaJson(TypedDict):
+    version: str
 
 
 def get_version() -> str:
-    return loads((Path(__file__).parent / "lerna.json").read_text())["version"]
+    return cast(LernaJson, loads((Path(__file__).parent / "lerna.json").read_text()))[
+        "version"
+    ]
--- pdm_build.py
+++ pdm_build.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from json import loads
 from pathlib import Path
 from shutil import copyfile, copytree
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypedDict, cast
 
 # https://github.com/samwillis/nodejs-pypi/pull/23
 if TYPE_CHECKING:
@@ -11,6 +11,11 @@ if TYPE_CHECKING:
     from subprocess import run  # noqa: S404
 else:
     from nodejs.npm import run
+
+
+class PackageJson(TypedDict):
+    bin: dict[str, str]
+
 
 if not Path("node_modules").exists():
     _ = run(["ci"], check=True)
@@ -20,7 +25,7 @@ npm_package_dir = Path("packages/pyright")
 pypi_package_dir = Path("basedpyright")
 
 copytree(npm_package_dir / "dist", pypi_package_dir / "dist", dirs_exist_ok=True)
-for script_path in loads((npm_package_dir / "package.json").read_text())[
-    "bin"
-].values():
-    copyfile(npm_package_dir / script_path, pypi_package_dir / script_path)
+for script_path in cast(
+    PackageJson, loads((npm_package_dir / "package.json").read_text())
+)["bin"].values():
+    _ = copyfile(npm_package_dir / script_path, pypi_package_dir / script_path)
