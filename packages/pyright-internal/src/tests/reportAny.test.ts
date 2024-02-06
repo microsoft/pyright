ebc66fc23e3b5e0fcffd3b01e3aa7ea0a4a670f0
DetachHead <detachhead@users.noreply.github.com>
2024-02-06 17:07:17 +1000

    add tests for `reportAny`

---
+++ packages/pyright-internal/src/tests/reportAny.test.ts
@@ -0,0 +1,46 @@
+import { ConfigOptions } from '../common/configOptions';
+import { DiagnosticRule } from '../common/diagnosticRules';
+import { Uri } from '../common/uri/uri';
+import { LocMessage } from '../localization/localize';
+import { typeAnalyzeSampleFiles, validateResultsButBased } from './testUtils';
+
+test('reportAny', () => {
+    const configOptions = new ConfigOptions(Uri.empty());
+    configOptions.diagnosticRuleSet.reportAny = 'error';
+    const analysisResults = typeAnalyzeSampleFiles(['reportAny.py'], configOptions);
+
+    validateResultsButBased(analysisResults, {
+        errors: [
+            { line: 0, code: DiagnosticRule.reportAny, message: LocMessage.typeAny().format({ name: 'Any' }) },
+            { line: 3, code: DiagnosticRule.reportAny, message: LocMessage.returnTypeAny() },
+            {
+                line: 3,
+                code: DiagnosticRule.reportAny,
+                message: LocMessage.paramTypeAny().format({ paramName: 'bar' }),
+            },
+            { line: 4, code: DiagnosticRule.reportAny },
+            { line: 5, code: DiagnosticRule.reportAny, message: LocMessage.returnTypeAny() },
+            { line: 7, code: DiagnosticRule.reportAny, message: LocMessage.typeAny().format({ name: 'bar' }) },
+            {
+                line: 9,
+                code: DiagnosticRule.reportAny,
+                message: LocMessage.classDecoratorTypeAny(),
+            },
+            {
+                line: 10,
+                code: DiagnosticRule.reportAny,
+                message: LocMessage.baseClassAny(),
+            },
+            {
+                line: 12,
+                code: DiagnosticRule.reportAny,
+                message: LocMessage.lambdaReturnTypeAny(),
+            },
+            {
+                line: 15,
+                code: DiagnosticRule.reportAny,
+                message: LocMessage.wildcardPatternTypeAny(),
+            },
+        ],
+    });
+});
---
+++ packages/pyright-internal/src/tests/samples/reportAny.py
@@ -0,0 +1,17 @@
+from typing import Any, Callable
+
+
+def foo(bar: Any) -> Any:
+    print(bar)
+    return bar
+
+bar: Any = object()
+
+@bar
+class Bar(bar): ...
+
+baz: Callable[[], Any] = lambda: bar
+
+match(bar):
+    case _:
+        ...
