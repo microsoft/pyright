6149e25c436d091911634a371f087542da11cb29
Eric Traut <eric@traut.com>
2024-02-12 00:39:52 -0800

    Fixed bug that results in a false positive "no overload implementation" error if an overloaded function is passed through a decorator that uses a ParamSpec. This addresses #7221. (#7246)

--- packages/pyright-internal/src/tests/samples/overload4.py
+++ packages/pyright-internal/src/tests/samples/overload4.py
@@ -1,23 +1,23 @@
 # This sample verifies that a lone overload is reported
 # as an error.
 
-from typing import Protocol, overload
+from typing import Any, Callable, ParamSpec, Protocol, TypeVar, overload
+
+T = TypeVar("T")
+P = ParamSpec("P")
 
 
 # This should generate an error because there is only one overload.
 @overload
-def func1() -> None:
-    ...
+def func1() -> None: ...
 
 
-def func1() -> None:
-    ...
+def func1() -> None: ...
 
 
 # This should generate an error because there is only one overload.
 @overload
-def func2(a: int) -> None:
-    ...
+def func2(a: int) -> None: ...
 
 
 def func2(a: int) -> None:
@@ -27,20 +27,36 @@ def func2(a: int) -> None:
 class ClassA:
     # This should generate an error because there is no implementation.
     @overload
-    def func3(self) -> None:
-        ...
+    def func3(self) -> None: ...
 
     @overload
-    def func3(self, a: int) -> None:
-        ...
+    def func3(self, a: int) -> None: ...
 
 
 class ClassB(Protocol):
     # An implementation should not be required in a protocol class.
     @overload
-    def func4(self) -> None:
-        ...
+    def func4(self) -> None: ...
 
     @overload
-    def func4(self, name: str) -> str:
-        ...
+    def func4(self, name: str) -> str: ...
+
+
+def deco1(
+    _origin: Callable[P, T]
+) -> Callable[[Callable[..., Any]], Callable[P, T]]: ...
+
+
+@overload
+def func5(v: int) -> int: ...
+
+
+@overload
+def func5(v: str) -> str: ...
+
+
+def func5(v: int | str) -> int | str: ...
+
+
+@deco1(func5)
+def func6(*args: Any, **kwargs: Any) -> Any: ...
