a8acb3f7b9636e0f1bcef848bbc437bfa59567bb
Eric Traut <eric@traut.com>
2024-02-27 19:58:06 -0700

    Fixed a bug in the import resolver that resulted in an incorrect resolution when an import within a `__init__.py` file uses the form `from .a import a`. This addresses #7233.

---
+++ packages/pyright-internal/src/tests/fourslash/import.nameconflict.fourslash.ts
@@ -0,0 +1,20 @@
+/// <reference path="fourslash.ts" />
+
+// @filename: project/__init__.py
+//// from .a import *
+
+// @filename: project/a/__init__.py
+//// from .b import b
+
+// @filename: project/a/b.py
+//// def b() -> None:
+////     pass
+
+// @filename: project/a/test.py
+//// from project import a
+//// x: a.[|/*marker*/b|]
+
+// @ts-ignore
+helper.verifyHover('markdown', {
+    marker: '```python\n(function) def b() -> None\n```',
+});
