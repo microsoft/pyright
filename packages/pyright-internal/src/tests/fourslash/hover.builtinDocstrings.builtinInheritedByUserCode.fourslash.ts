51af41bd7d1d77ed2e710a80216d03ed990f826d
Erik De Bonte <erikd@microsoft.com>
2024-03-05 17:57:13 -0800

    Unit tests

---
+++ packages/pyright-internal/src/tests/fourslash/hover.builtinDocstrings.builtinInheritedByUserCode.fourslash.ts
@@ -0,0 +1,22 @@
+/// <reference path="fourslash.ts" />
+
+// @filename: typeshed-fallback/stdlib/builtins.pyi
+//// class baseClass:
+////     def method(self) -> None: ...
+
+// @filename: typeshed-fallback/stdlib/builtins.py
+//// class baseClass:
+////     def method(self) -> None:
+////         """baseClass doc string"""
+////         pass
+
+// @filename: test.py
+//// class derivedClass(baseClass):
+////     pass
+////
+//// x = derivedClass()
+//// x.[|/*marker*/method|]()
+
+helper.verifyHover('markdown', {
+    marker: '```python\n(method) def method() -> None\n```',
+});
