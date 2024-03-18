3097df676e62185599f36c6d3ff44c822e653ff0
DetachHead <detachhead@users.noreply.github.com>
2024-03-18 13:01:17 +1000

    add inlay hint test for new type alias syntax

--- packages/pyright-internal/src/tests/samples/inlay_hints/variables.py
+++ packages/pyright-internal/src/tests/samples/inlay_hints/variables.py
@@ -6,4 +6,5 @@ T = TypeVar(name="T") # no inlay hint because typevar
 U = TypeVar(name="U", bound=Function) # no inlay hint because typevar
 P = ParamSpec(name="P") # no inlay hint because paramspec
 _ = str(1)  # no inlay hint because underscore only variable
-Foo = int # inlay hint of "TypeAlias"
+Foo = int  # inlay hint of "TypeAlias"
+type Bar = str  # no inlay hint
