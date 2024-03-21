c0eb65b9a2f21c5d2582471a3d244114d06b009d
Erik De Bonte <erikd@microsoft.com>
2024-03-21 11:44:24 -0700

    Added comments and ran black

--- build/generateUnicodeTables.py
+++ build/generateUnicodeTables.py
@@ -42,7 +42,7 @@ def parseFile(filePath: str) -> list[Character]:
             category = splitOnSemicolon[2]
 
             if splitOnSemicolon[1].endswith(", First>"):
-                # Range syntax
+                # Legacy range syntax
                 # D800;<Non Private Use High Surrogate, First>;Cs;0;L;;;;;N;;;;;
                 # DB7F;<Non Private Use High Surrogate, Last>;Cs;0;L;;;;;N;;;;;
                 nextLine = lines[i + 1]
@@ -58,6 +58,13 @@ def parseFile(filePath: str) -> list[Character]:
         return chars
 
 
+# Given a collection of characters, returns a list of ranges of contiguous
+# characters. Contiguous means that the character codes are sequential with
+# no gaps and the characters all have the same category. For character codes
+# greater than 0xFFFF, contiguous means that the high surrogate is the same
+# and the low surrogate values are sequential with no gaps. So, two charcter
+# codes might be sequential numerically but have different high surrogates,
+# and therefore would not be members of the same range.
 def getSurrogateRanges(chars: list[Character]) -> list[CharacterRange]:
     surrogateRanges: list[CharacterRange] = []
 
@@ -94,6 +101,10 @@ def getSurrogateRanges(chars: list[Character]) -> list[CharacterRange]:
     return surrogateRanges
 
 
+# Write out a table of all character codes within the specified category. These are
+# the full hex character codes (Unicode code points) not surrogate values. Sequential
+# ranges of character codes are written as arrays of two numbers (start and end) to
+# save space.
 def writeRangeTable(writer: TextIOWrapper, category: str, chars: list[Character]):
     chars = [ch for ch in chars if ch.category == category]
 
@@ -117,6 +128,9 @@ def writeRangeTable(writer: TextIOWrapper, category: str, chars: list[Character]
     writer.write("];\n\n")
 
 
+# Write out a table of all characters within the specified category using their UTF-16
+# values. Characters are grouped by high surrogate value. Sequential ranges of low
+# surrogate values are written as arrays of two numbers (start and end) to save space.
 def writeSurrogateRangeTable(
     writer: TextIOWrapper, category: str, surrogateRanges: list[CharacterRange]
 ):
@@ -159,7 +173,8 @@ chars = parseFile(path)
 surrogateRanges = getSurrogateRanges(chars)
 
 with open("packages/pyright-internal/src/parser/unicode.ts", "w") as writer:
-    writer.write(f"""/*
+    writer.write(
+        f"""/*
  * unicode.ts
  * Copyright (c) Microsoft Corporation.
  * Licensed under the MIT license.
@@ -175,7 +190,8 @@ export type UnicodeRange = [number, number] | number;
 export type UnicodeRangeTable = UnicodeRange[];
 export type UnicodeSurrogateRangeTable = {{ [surrogate: number]: UnicodeRange[] }};
 
-""")
+"""
+    )
 
     for category in ["Lu", "Ll", "Lt", "Lo", "Lm", "Nl", "Mn", "Mc", "Nd", "Pc"]:
         writeRangeTable(writer, category, chars)
