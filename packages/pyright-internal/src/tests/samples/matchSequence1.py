476447cb651817bbebbe528af18ae77057320d8e
Eric Traut <eric@traut.com>
2024-04-04 10:06:35 -0700

    Fixed a bug that results in incorrect type evaluation when a sequence pattern in a `match` statement includes a `*` element and the subject includes a tuple with an element with indeterminate length. This addresses #7613. (#7616)

--- packages/pyright-internal/src/tests/samples/matchSequence1.py
+++ packages/pyright-internal/src/tests/samples/matchSequence1.py
@@ -479,3 +479,12 @@ def test_unbounded_tuple(
             reveal_type(x, expected_text="int")
             reveal_type(y, expected_text="str")
             reveal_type(z, expected_text="complex")
+
+
+def test_unbounded_tuple_2(subj: tuple[int, str, Unpack[tuple[range, ...]]]) -> None:
+    match subj:
+        case [1, *ts1]:
+            reveal_type(ts1, expected_text="list[str | range]")
+
+        case [1, "", *ts2]:
+            reveal_type(ts2, expected_text="list[range]")
