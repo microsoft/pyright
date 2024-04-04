d34373198ba7fe05b8a7dffe7ca4abeda10919c0
Eric Traut <eric@traut.com>
2024-04-04 09:57:58 -0700

    Fixed a bug that results in incorrect type evaluation when a sequence pattern in a `match` statement includes a `*` element and the subject includes a tuple with an element with indeterminate length. This addresses #7613.

--- packages/pyright-internal/src/analyzer/patternMatching.ts
+++ packages/pyright-internal/src/analyzer/patternMatching.ts
@@ -1290,7 +1290,7 @@ function getSequencePatternInfo(
                             typeArgs.splice(tupleIndeterminateIndex, 0, typeArgs[tupleIndeterminateIndex]);
                         }
 
-                        if (typeArgs.length > patternEntryCount) {
+                        if (typeArgs.length > patternEntryCount && patternStarEntryIndex === undefined) {
                             typeArgs.splice(tupleIndeterminateIndex, 1);
                         }
                     }
