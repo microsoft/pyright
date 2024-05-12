6d97e14ed0b47c0c037cb16fb38e454f0ed6f657
Eric Traut <eric@traut.com>
2024-05-12 15:53:57 -0700

    Changed the type evaluation for `type(Any)` or `type(x)` where `x` is a value of type `Any` or `Unknown`. These now evaluate to `type[Unknown]`.

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -9710,23 +9710,29 @@ export function createTypeEvaluator(
                 if (expandedCallType.details.name === 'type' && argList.length === 1) {
                     const argType = getTypeOfArgument(argList[0]).type;
                     const returnType = mapSubtypes(argType, (subtype) => {
-                        if (isInstantiableClass(subtype)) {
-                            return subtype.details.effectiveMetaclass ?? AnyType.create();
+                        if (isInstantiableClass(subtype) && subtype.details.effectiveMetaclass) {
+                            return subtype.details.effectiveMetaclass;
                         }
 
-                        if (
-                            isClassInstance(subtype) ||
-                            (isTypeVar(subtype) && TypeBase.isInstance(subtype)) ||
-                            isNoneInstance(subtype)
-                        ) {
-                            return convertToInstantiable(stripLiteralValue(subtype));
+                        if (isNever(subtype)) {
+                            return subtype;
                         }
 
-                        if (isFunction(subtype) && TypeBase.isInstance(subtype)) {
-                            return FunctionType.cloneAsInstantiable(subtype);
+                        if (TypeBase.isInstance(subtype)) {
+                            if (isClass(subtype) || isTypeVar(subtype)) {
+                                return convertToInstantiable(stripLiteralValue(subtype));
+                            }
+
+                            if (isFunction(subtype)) {
+                                return FunctionType.cloneAsInstantiable(subtype);
+                            }
                         }
 
-                        return AnyType.create();
+                        return ClassType.cloneForSpecialization(
+                            ClassType.cloneAsInstance(expandedCallType),
+                            [UnknownType.create()],
+                            /* isTypeArgumentExplicit */ true
+                        );
                     });
 
                     return { returnType };
