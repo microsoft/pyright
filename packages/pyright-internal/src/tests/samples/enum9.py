2dffecb0f2a20c918ba966c20ec94cc76c188472
Eric Traut <eric@traut.com>
2024-07-11 20:31:19 -0700

    Added support for class declarations within an enum class body -- both with and without `@member` and `@nonmember` decorators. This addresses #8366. (#8385)

--- packages/pyright-internal/src/tests/samples/enum9.py
+++ packages/pyright-internal/src/tests/samples/enum9.py
@@ -1,8 +1,9 @@
 # This sample tests the enum.member and enum.nonmember classes introduced
 # in Python 3.11.
 
+import sys
 from enum import Enum, member, nonmember
-from typing import ClassVar, Literal
+from typing import Literal
 
 
 class Enum1(Enum):
@@ -15,13 +16,35 @@ class Enum1(Enum):
     def ALSO_A_MEMBER() -> Literal[4]:
         return 4
 
+    @member
+    class ClassA:
+        pass
+
+    @nonmember
+    class ClassB:
+        pass
+
+    class ClassC:
+        pass
+
 
 reveal_type(Enum1.MEMBER, expected_text="Literal[Enum1.MEMBER]")
 reveal_type(Enum1.ANOTHER_MEMBER, expected_text="Literal[Enum1.ANOTHER_MEMBER]")
 reveal_type(Enum1.ALSO_A_MEMBER, expected_text="Literal[Enum1.ALSO_A_MEMBER]")
 reveal_type(Enum1.NON_MEMBER, expected_text="int")
+reveal_type(Enum1.ClassA, expected_text="Literal[Enum1.ClassA]")
+reveal_type(Enum1.ClassB, expected_text="type[ClassB]")
+
+if sys.version_info >= (3, 13):
+    reveal_type(Enum1.ClassC, expected_text="type[ClassC]")
+else:
+    reveal_type(Enum1.ClassC, expected_text="Literal[Enum1.ClassC]")
 
 
 reveal_type(Enum1.MEMBER.value, expected_text="Literal[1]")
 reveal_type(Enum1.ANOTHER_MEMBER.value, expected_text="int")
 reveal_type(Enum1.ALSO_A_MEMBER.value, expected_text="() -> Literal[4]")
+reveal_type(Enum1.ClassA.value, expected_text="type[ClassA]")
+
+if sys.version_info < (3, 13):
+    reveal_type(Enum1.ClassC.value, expected_text="type[ClassC]")
