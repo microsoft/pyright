0683551867b4224d0f5ced6ad0a017161b3d6aca
Eric Traut <eric@traut.com>
2024-07-20 19:05:02 -0700

    Removed redundant assignment flag.

--- packages/pyright-internal/src/analyzer/typeGuards.ts
+++ packages/pyright-internal/src/analyzer/typeGuards.ts
@@ -1415,9 +1415,7 @@ function narrowTypeForIsInstanceInternal(
                                 /* diag */ undefined,
                                 /* destTypeVarContext */ undefined,
                                 /* srcTypeVarContext */ undefined,
-                                AssignTypeFlags.IgnoreTypeVarScope |
-                                    AssignTypeFlags.SkipProtocolAssignmentCheck |
-                                    AssignTypeFlags.AllowIsinstanceSpecialForms
+                                AssignTypeFlags.IgnoreTypeVarScope | AssignTypeFlags.AllowIsinstanceSpecialForms
                             )
                         ) {
                             // If the variable type is a superclass of the isinstance
