4fc5de5c1e18ed5a26bad0746b112612966b0db9
Eric Traut <eric@traut.com>
2024-08-10 00:47:49 -0600

    Fixed a false positive error when solving type variables in a call that involves a lambda. This addresses #8714. (#8730)

---
+++ packages/pyright-internal/src/tests/samples/solver40.py
@@ -0,0 +1,16 @@
+# This sample involves solving type variables that provide a lambda
+# its expected type.
+
+# pyright: strict
+
+from typing import Callable, TypeVar
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+
+def func1(lst: list[T], init: U, f: Callable[[U, T], U]) -> U:
+    ...
+
+
+y = func1([1], 1, lambda x, y: x * y)
