b79badb36b88e861483378a686a26c492f1c8cc4
Eric Traut <eric@traut.com>
2024-09-08 16:04:58 -0700

    Fixed a bug that leads to incorrect type narrowing with `isinstance` or `TypeIs` when the filter type (the second argument to `isinstance`) is a subclass of `type`. This addresses #8691 and #8686.

---
+++ packages/pyright-internal/src/tests/samples/typeNarrowingIsinstance21.py
@@ -0,0 +1,28 @@
+# This sample tests the case where the filter type is a class object.
+
+# pyright: reportMissingModuleSource=false
+
+from typing_extensions import TypeIs
+
+
+class Sentinel:
+    pass
+
+
+def is_sentinel(value: object) -> TypeIs[type[Sentinel]]: ...
+
+
+def _(a: dict[str, int] | type[Sentinel]):
+    if is_sentinel(a):
+        reveal_type(a, expected_text="type[Sentinel]")
+    else:
+        reveal_type(a, expected_text="dict[str, int]")
+
+
+def is_str_type(typ: object) -> TypeIs[type[str]]:
+    return typ is str
+
+
+def test_typevar[T](typ: type[T], val: T) -> None:
+    if is_str_type(typ):
+        reveal_type(typ, expected_text="type[str]*")
