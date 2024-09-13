cce80c35fbb0b4a151ee2a3a22202f8d8f4a7b56
Eric Traut <eric@traut.com>
2024-09-13 15:26:51 -0700

    Made adjustments to `TypeForm` behaviors based on feedback on draft PEP 747.

--- packages/pyright-internal/src/tests/samples/typeForm5.py
+++ packages/pyright-internal/src/tests/samples/typeForm5.py
@@ -2,7 +2,7 @@
 
 # pyright: reportMissingModuleSource=false
 
-from typing import Literal, LiteralString, Optional, TypeGuard
+from typing import Literal, LiteralString, TypeGuard, Optional
 from typing_extensions import TypeForm, TypeIs
 
 
@@ -15,7 +15,7 @@ def func2[S](x: TypeForm[S | None]) -> S: ...
 def func3[S, T](x: TypeForm[S | T]) -> S: ...
 
 
-def func4[T](x: TypeForm[T]) -> TypeIs[T]: ...
+def func4[T](x: object, t: TypeForm[T]) -> TypeIs[T]: ...
 
 
 def func5[T](x: TypeForm[T]) -> TypeGuard[type[T]]: ...
@@ -28,10 +28,10 @@ v2 = func1("int | str")
 reveal_type(v2, expected_text="int | str")
 
 v3 = func1(LiteralString)
-reveal_type(v3, expected_text="str")
+reveal_type(v3, expected_text="LiteralString")
 
 v4 = func1(Literal[1, 2, 3])
-reveal_type(v4, expected_text="int")
+reveal_type(v4, expected_text="Literal[1, 2, 3]")
 
 v5 = func2("Optional[str]")
 reveal_type(v5, expected_text="str")
@@ -42,7 +42,7 @@ reveal_type(v6, expected_text="int | str")
 v7 = func3(int | str | None)
 reveal_type(v7, expected_text="int | str | None")
 
-v8 = func4(int | str | None)
+v8 = func4(1, int | str | None)
 reveal_type(v8, expected_text="TypeIs[int | str | None]")
 
 v9 = func5(int | str | None)
