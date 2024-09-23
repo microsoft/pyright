06878da845f0f91c9f1f7ae699ab34232dd55f5f
Eric Traut <eric@traut.com>
2024-09-23 08:27:16 -0700

    Fixed a bug that results in a false positive when assigning `Never` to a type variable in a contravariant context. This addresses #9056. (#9061)

--- packages/pyright-internal/src/tests/samples/never2.py
+++ packages/pyright-internal/src/tests/samples/never2.py
@@ -6,6 +6,8 @@ from typing import Generic, Never, TypeVar
 U = TypeVar("U")
 
 T_co = TypeVar("T_co", covariant=True)
+T_contra = TypeVar("T_contra", contravariant=True)
+T = TypeVar("T")
 
 
 class ClassA(Generic[T_co]):
@@ -16,9 +18,6 @@ def func1(x: U) -> ClassA[U]:
     return ClassA[Never]()
 
 
-T = TypeVar("T")
-
-
 class ClassB(Generic[T]):
     pass
 
@@ -26,3 +25,11 @@ class ClassB(Generic[T]):
 def func2(x: U) -> ClassB[U]:
     # This should generate an error because T is invariant.
     return ClassB[Never]()
+
+
+class ClassC(Generic[T_contra]):
+    def __init__(self, x: T_contra): ...
+
+
+def func3(x: U) -> U | ClassC[Never]:
+    return ClassC(x)
