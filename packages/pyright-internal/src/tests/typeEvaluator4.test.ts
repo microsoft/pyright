14288f8a6357b372398710b8bbfd7ea71a81e1e8
Eric Traut <eric@traut.com>
2024-10-06 10:39:11 -0700

    Fixed a bug that leads to a false negative when a class-scoped variable is annotated with a value-constrained type variable and assigned a default value with one of the value constraints. This addresses https://github.com/microsoft/pylance-release/issues/6530. (#9156)

--- packages/pyright-internal/src/tests/typeEvaluator4.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluator4.test.ts
@@ -267,7 +267,7 @@ test('DataClassNamedTuple2', () => {
 test('DataClass1', () => {
     const analysisResults = TestUtils.typeAnalyzeSampleFiles(['dataclass1.py']);
 
-    TestUtils.validateResults(analysisResults, 7);
+    TestUtils.validateResults(analysisResults, 9);
 });
 
 test('DataClass2', () => {
