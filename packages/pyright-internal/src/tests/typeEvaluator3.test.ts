630dfc20d32bc0dd9305cffa6bd5d2de117da8d4
Eric Traut <eric@traut.com>
2024-11-07 14:07:01 -0800

    Added missing checks for illegal use of `await` and `async` within list, set and dictionary comprehensions within a non-async function. This addresses #9414. (#9421)

--- packages/pyright-internal/src/tests/typeEvaluator3.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluator3.test.ts
@@ -151,6 +151,12 @@ test('Await2', () => {
     TestUtils.validateResults(analysisResults, 0);
 });
 
+test('Await3', () => {
+    const analysisResults = TestUtils.typeAnalyzeSampleFiles(['await3.py']);
+
+    TestUtils.validateResults(analysisResults, 6);
+});
+
 test('Coroutines1', () => {
     const configOptions = new ConfigOptions(Uri.empty());
 
