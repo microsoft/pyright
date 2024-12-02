a2c380ecb4c005f3cea4db2fb5e552eccff8fae3
Eric Traut <eric@traut.com>
2024-12-02 05:08:46 -0800

    Improved dataclass converters to support generic types. This addresses #9512. (#9528)

--- packages/pyright-internal/src/tests/typeEvaluator4.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluator4.test.ts
@@ -441,6 +441,12 @@ test('DataClassConverter2', () => {
     TestUtils.validateResults(analysisResults, 4);
 });
 
+test('DataClassConverter3', () => {
+    const analysisResults = TestUtils.typeAnalyzeSampleFiles(['dataclassConverter3.py']);
+
+    TestUtils.validateResults(analysisResults, 0);
+});
+
 test('DataClassPostInit1', () => {
     const analysisResults = TestUtils.typeAnalyzeSampleFiles(['dataclassPostInit1.py']);
 
