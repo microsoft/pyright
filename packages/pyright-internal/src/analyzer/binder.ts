722376c6b5ab0fecb76f223f7d561fc0003367fd
Eric Traut <eric@traut.com>
2025-01-28 12:23:13 -0800

    Tweaked the algorithm for computing the complexity of a code flow graph to accommodate slightly larger graphs before giving up. This addresses #9778. (#9781)

--- packages/pyright-internal/src/analyzer/binder.ts
+++ packages/pyright-internal/src/analyzer/binder.ts
@@ -159,7 +159,7 @@ interface NarrowExprOptions {
 // amount to the complexity factor. Without this, the complexity
 // calculation fails to take into account large numbers of non-cyclical
 // flow nodes. This number is somewhat arbitrary and is tuned empirically.
-const flowNodeComplexityContribution = 0.05;
+const flowNodeComplexityContribution = 0.025;
 
 export class Binder extends ParseTreeWalker {
     private readonly _fileInfo: AnalyzerFileInfo;
@@ -4087,17 +4087,25 @@ export class Binder extends ParseTreeWalker {
                 // a decorator that tells us otherwise.
                 isInstanceMember = true;
                 for (const decorator of methodNode.d.decorators) {
-                    if (decorator.d.expr.nodeType === ParseNodeType.Name) {
-                        const decoratorName = decorator.d.expr.d.value;
+                    let decoratorName: string | undefined;
 
-                        if (decoratorName === 'staticmethod') {
-                            // A static method doesn't have a "self" or "cls" parameter.
-                            return undefined;
-                        } else if (decoratorName === 'classmethod') {
-                            // A classmethod implies that the first parameter is "cls".
-                            isInstanceMember = false;
-                            break;
-                        }
+                    if (decorator.d.expr.nodeType === ParseNodeType.Name) {
+                        decoratorName = decorator.d.expr.d.value;
+                    } else if (
+                        decorator.d.expr.nodeType === ParseNodeType.MemberAccess &&
+                        decorator.d.expr.d.leftExpr.nodeType === ParseNodeType.Name &&
+                        decorator.d.expr.d.leftExpr.d.value === 'builtins'
+                    ) {
+                        decoratorName = decorator.d.expr.d.member.d.value;
+                    }
+
+                    if (decoratorName === 'staticmethod') {
+                        // A static method doesn't have a "self" or "cls" parameter.
+                        return undefined;
+                    } else if (decoratorName === 'classmethod') {
+                        // A classmethod implies that the first parameter is "cls".
+                        isInstanceMember = false;
+                        break;
                     }
                 }
             }
