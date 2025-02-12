75889da88518e894c2cef326614cab1502781d97
Eric Traut <eric@traut.com>
2025-02-11 20:52:56 -0800

    Fixed a bug that results in incomplete renames when renaming a class or function that uses PEP 695 type parameter syntax. This addresses https://github.com/microsoft/pylance-release/issues/6893. (#9893)

---
+++ packages/pyright-internal/src/tests/fourslash/rename.typeParams.fourslash.ts
@@ -0,0 +1,29 @@
+/// <reference path="typings/fourslash.d.ts" />
+
+// @filename: pyrightconfig.json
+//// {
+////   "useLibraryCodeForTypes": true
+//// }
+
+// @filename: test.py
+//// class [|Test1|][T]:
+////    def M(self, a: '[|Test1|]'):
+////     pass
+
+// @filename: test2.py
+//// from test import [|Test1|]
+////
+//// b = [|[|/*marker*/Test1|]|]()
+
+{
+    const ranges = helper.getRanges().filter((r) => !r.marker);
+
+    helper.verifyRename({
+        marker: {
+            newName: 'NewTest1',
+            changes: ranges.map((r) => {
+                return { filePath: r.fileName, range: helper.convertPositionRange(r), replacementText: 'NewTest1' };
+            }),
+        },
+    });
+}
