1c2cc56f543b176e4eb6880e46a5677a155834fd
Eric Traut <eric@traut.com>
2025-02-25 11:15:46 -0700

    Fixed a bug that results in an incorrect error when determining assignability of a function with an `*args` that is unpacked using a `*tuple[]`. This addresses #9974. (#9975)

--- packages/pyright-internal/src/tests/samples/callbackProtocol7.py
+++ packages/pyright-internal/src/tests/samples/callbackProtocol7.py
@@ -4,13 +4,33 @@
 from typing import Any, Protocol
 
 
-class X(Protocol):
-    def __call__(self, x: int, /, y: str) -> Any:
-        ...
+class P0(Protocol):
+    def __call__(self, x: int, /, y: str) -> Any: ...
 
 
-def f1(x: int, /, y: str, z: None = None) -> Any:
-    ...
+def test1(x: int, /, y: str, z: None = None) -> Any: ...
 
 
-x: X = f1
+x: P0 = test1
+
+
+class P1(Protocol):
+    def __call__(self, *args: *tuple[int, int]): ...
+
+
+class P2(Protocol):
+    def __call__(self, x: int, y: int, /): ...
+
+
+class P3(Protocol):
+    def __call__(self, x: int, /, *args: *tuple[int]): ...
+
+
+class P4(Protocol):
+    def __call__(self, x: int, y: int = 2, /): ...
+
+
+def test2(p1: P1, p2: P2, p3: P3, p4: P4):
+    x1: P1 = p2
+    x2: P1 = p3
+    x3: P1 = p4
