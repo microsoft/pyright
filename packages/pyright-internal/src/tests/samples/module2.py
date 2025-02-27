861abd9a0333b532da16c308bcb7c10c1f1d3030
Eric Traut <eric@traut.com>
2025-02-27 00:14:39 -0700

    Added bidirectional type inference for container types that are parameterized by a protocol that matches a module. This addresses #9988. (#9989)

--- packages/pyright-internal/src/tests/samples/module2.py
+++ packages/pyright-internal/src/tests/samples/module2.py
@@ -3,6 +3,7 @@
 
 import typing
 import importlib
+from typing import Protocol
 from types import ModuleType
 
 importlib.reload(typing)
@@ -23,3 +24,17 @@ reveal_type(v2, expected_text="dict[ModuleType, ModuleType]")
 
 v3 = (importlib, typing)
 reveal_type(v3, expected_text="tuple[ModuleType, ModuleType]")
+
+
+class ModuleProto(Protocol):
+    def reload(self, module: ModuleType) -> ModuleType: ...
+
+
+v4: ModuleProto = importlib
+reveal_type(v4, expected_text='Module("importlib")')
+
+v5: tuple[ModuleProto] = (importlib,)
+reveal_type(v5, expected_text='tuple[Module("importlib")]')
+
+v6: list[ModuleProto] = [importlib]
+reveal_type(v6, expected_text="list[ModuleProto]")
