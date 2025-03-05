a44bde34713d3c2a42aa049d8af0f19e4630b8e7
Eric Traut <eric@traut.com>
2025-03-05 12:14:34 -0700

    Improved support for unused import detection for import statements that target multi-part names. This addresses #9622.

---
+++ packages/pyright-internal/src/tests/samples/unusedImport2.py
@@ -0,0 +1,16 @@
+# This sample tests the reportUnusedImport diagnostic rule
+# for multipart imports.
+
+# This should result in an error if reportUnusedImport is enabled.
+import package2.module1
+
+# This should result in an error if reportUnusedImport is enabled.
+import package2.module2
+
+import package2.module3
+
+import package2.module2 as dummy
+
+def func1():
+    print(package2.module3.a3)
+    print(dummy)
