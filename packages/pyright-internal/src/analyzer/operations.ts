b50d3a8d89c0e1473c1afeecb8f62661109cae4a
Eric Traut <eric@traut.com>
2025-03-13 10:49:58 -0600

    Fixed a bug that results in incorrect "literal math" results when using a floor divide `//` operator with literal operands whose signs differ. This addresses #10076.

--- packages/pyright-internal/src/analyzer/operations.ts
+++ packages/pyright-internal/src/analyzer/operations.ts
@@ -1032,6 +1032,15 @@ function calcLiteralForBinaryOp(operator: OperatorType, leftType: Type, rightTyp
                     } else if (operator === OperatorType.FloorDivide) {
                         if (rightLiteralValue !== BigInt(0)) {
                             newValue = leftLiteralValue / rightLiteralValue;
+
+                            // BigInt rounds to zero, but floor divide rounds to negative
+                            // infinity, so we need to adjust the result if the signs
+                            // of the operands are different.
+                            if (leftLiteralValue !== rightLiteralValue) {
+                                if (leftLiteralValue < BigInt(0) !== rightLiteralValue < BigInt(0)) {
+                                    newValue -= BigInt(1);
+                                }
+                            }
                         }
                     } else if (operator === OperatorType.Mod) {
                         if (rightLiteralValue !== BigInt(0)) {
