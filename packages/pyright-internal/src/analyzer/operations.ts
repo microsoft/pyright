1876073f08cd1b2900df12d991c3ec2f6549fe44
Eric Traut <eric@traut.com>
2025-03-25 18:59:54 -0700

    Fixed bug in "literal math" logic for modulo and floor divide operators that affect cases where operands have different signs. This addresses #10167. (#10168)

--- packages/pyright-internal/src/analyzer/operations.ts
+++ packages/pyright-internal/src/analyzer/operations.ts
@@ -1036,18 +1036,19 @@ function calcLiteralForBinaryOp(operator: OperatorType, leftType: Type, rightTyp
                             // BigInt rounds to zero, but floor divide rounds to negative
                             // infinity, so we need to adjust the result if the signs
                             // of the operands are different.
-                            if (leftLiteralValue !== rightLiteralValue && leftLiteralValue !== BigInt(0)) {
-                                if (
-                                    leftLiteralValue < BigInt(0) !== rightLiteralValue < BigInt(0) &&
-                                    leftLiteralValue !== rightLiteralValue * BigInt(-1)
-                                ) {
-                                    newValue -= BigInt(1);
-                                }
+                            if (
+                                newValue * rightLiteralValue !== leftLiteralValue &&
+                                leftLiteralValue < BigInt(0) !== rightLiteralValue < BigInt(0)
+                            ) {
+                                newValue -= BigInt(1);
                             }
                         }
                     } else if (operator === OperatorType.Mod) {
                         if (rightLiteralValue !== BigInt(0)) {
-                            newValue = leftLiteralValue % rightLiteralValue;
+                            // BigInt always produces a remainder, but Python produces
+                            // a modulo result whose sign is always the same as the
+                            // right operand.
+                            newValue = ((leftLiteralValue % rightLiteralValue) + rightLiteralValue) % rightLiteralValue;
                         }
                     } else if (operator === OperatorType.Power) {
                         if (rightLiteralValue >= BigInt(0)) {
