ea409bd1cc61c0ab168a80357b2666ddb75e28e9
Eric Traut <eric@traut.com>
2025-04-14 13:15:14 -0700

    Reverted a recent change that extended the `reportPrivateImportUsage` check to enforce imports from submodules whose names begin with an underscore. This change was disruptive to users of the popular `pandas` library because it exports a submodule named `_typing`. This was originally intended to be experimental or private, but it was never fully addressed. The pandas maintainers will work to fix this issue (https://github.com/pandas-dev/pandas/issues/55231) over the next year. I'm going to back out this change to pyright in the meantime.

--- packages/pyright-internal/src/analyzer/programTypes.ts
+++ packages/pyright-internal/src/analyzer/programTypes.ts
@@ -9,7 +9,7 @@ import { ConsoleInterface } from '../common/console';
 import { LogTracker } from '../common/logTracker';
 import { ServiceProvider } from '../common/serviceProvider';
 import { Uri } from '../common/uri/uri';
-import { IPythonMode, SourceFile, SourceFileEditMode } from './sourceFile';
+import { SourceFileEditMode, IPythonMode, SourceFile } from './sourceFile';
 
 export interface ISourceFileFactory {
     createSourceFile(
@@ -18,7 +18,6 @@ export interface ISourceFileFactory {
         moduleName: string,
         isThirdPartyImport: boolean,
         isThirdPartyPyTypedPresent: boolean,
-        isModulePrivate: boolean,
         editMode: SourceFileEditMode,
         console?: ConsoleInterface,
         logTracker?: LogTracker,
