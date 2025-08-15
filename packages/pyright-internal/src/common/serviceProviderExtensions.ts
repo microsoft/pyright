de2d23d084c779ffc0cbb9c8f1644b06eb04f215
Eric Traut <eric@traut.com>
2025-08-14 23:36:29 -0700

    Improved startup performance for large projects by lazily computing the module name of each source file. (#10798)

--- packages/pyright-internal/src/common/serviceProviderExtensions.ts
+++ packages/pyright-internal/src/common/serviceProviderExtensions.ts
@@ -115,7 +115,7 @@ const DefaultSourceFileFactory: ISourceFileFactory = {
     createSourceFile(
         serviceProvider: ServiceProvider,
         fileUri: Uri,
-        moduleName: string,
+        moduleNameGetter: (file: Uri) => string,
         isThirdPartyImport: boolean,
         isThirdPartyPyTypedPresent: boolean,
         editMode: SourceFileEditMode,
@@ -126,7 +126,7 @@ const DefaultSourceFileFactory: ISourceFileFactory = {
         return new SourceFile(
             serviceProvider,
             fileUri,
-            moduleName,
+            moduleNameGetter,
             isThirdPartyImport,
             isThirdPartyPyTypedPresent,
             editMode,
