ed83d58de9314533c9f577edd72bd709242235e5
Mike Barry <jabbera@users.noreply.github.com>
2025-10-03 20:39:06 -0400

    Eagerly exit isEnumClassWithMembers (#10990)
    * Eagerly exit isEnumClassWithMembers
    
    * Fix linting error
    
    ---------
    
    Co-authored-by: Eric Traut <eric@traut.com>

--- packages/pyright-internal/src/analyzer/enums.ts
+++ packages/pyright-internal/src/analyzer/enums.ts
@@ -61,20 +61,19 @@ export function isEnumClassWithMembers(evaluator: TypeEvaluator, classType: Clas
     }
 
     // Determine whether the enum class defines a member.
-    let definesMember = false;
-
-    ClassType.getSymbolTable(classType).forEach((symbol, name) => {
+    const symbolTable = ClassType.getSymbolTable(classType);
+    for (const name of symbolTable.keys()) {
         const symbolType = transformTypeForEnumMember(evaluator, classType, name);
         if (
             symbolType &&
             isClassInstance(symbolType) &&
             ClassType.isSameGenericClass(symbolType, ClassType.cloneAsInstance(classType))
         ) {
-            definesMember = true;
+            return true;
         }
-    });
+    }
 
-    return definesMember;
+    return false;
 }
 
 // Creates a new custom enum class with named values.
