52ed01ce67fd25d20f839860cb48900d43d992a1
Henry Su <henrysu4707@gmail.com>
2026-08-08 12:44:36 -0500

    Refine TypeGuard argument resolution for class methods, module functions, and keyword args

--- packages/pyright-internal/src/tests/samples/typeGuardMethod1.py
+++ packages/pyright-internal/src/tests/samples/typeGuardMethod1.py
@@ -1,7 +1,8 @@
 # This sample tests TypeGuard and TypeIs narrowing for bound, unbound,
-# and static method calls.
+# static, reordered keyword, and module-qualified function calls.
 
 from typing import Any, TypeGuard, TypeIs
+import typeGuard1
 
 
 class Checker:
@@ -23,6 +24,13 @@ def test_bound_method(c: Checker, x: object):
         reveal_type(x, expected_text="object")
 
 
+def test_bound_method_keyword(c: Checker, x: object):
+    if c.is_str(val=x):
+        reveal_type(x, expected_text="str")
+    else:
+        reveal_type(x, expected_text="object")
+
+
 def test_bound_method_typeis(c: Checker, x: int | str):
     if c.is_int(x):
         reveal_type(x, expected_text="int")
@@ -37,6 +45,15 @@ def test_unbound_method(c: Checker, x: object):
         reveal_type(x, expected_text="object")
 
 
+def test_unbound_method_keyword(c: Checker, x: object):
+    if Checker.is_str(val=x, self=c):
+        reveal_type(x, expected_text="str")
+        reveal_type(c, expected_text="Checker")
+    else:
+        reveal_type(x, expected_text="object")
+        reveal_type(c, expected_text="Checker")
+
+
 def test_unbound_method_typeis(c: Checker, x: int | str):
     if Checker.is_int(c, x):
         reveal_type(x, expected_text="int")
@@ -49,3 +66,10 @@ def test_static_method(x: object):
         reveal_type(x, expected_text="float")
     else:
         reveal_type(x, expected_text="object")
+
+
+def test_module_qualified_free_function(a: tuple[int, ...]):
+    if typeGuard1.is_two_element_tuple(a):
+        reveal_type(a, expected_text="tuple[int, int]")
+    else:
+        reveal_type(a, expected_text="tuple[int, ...]")
