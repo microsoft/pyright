931274777589ebec359c619960947576f83ceb94
Henry Su <henrysu4707@gmail.com>
2026-08-11 16:32:10 -0500

    Restrict negative tuple membership elimination of instantiable classes to final classes

--- packages/pyright-internal/src/tests/samples/typeNarrowingContainer1.py
+++ packages/pyright-internal/src/tests/samples/typeNarrowingContainer1.py
@@ -1,20 +1,38 @@
 # This sample tests negative type narrowing for tuple membership checks (in / not in)
 # containing instantiable class objects (type[T]).
 
+from typing import final
 from typing_extensions import assert_type
 
-class ClassA: pass
-class ClassB: pass
+@final
+class FinalClassA: pass
+
+@final
+class FinalClassB: pass
+
 class ClassC: pass
 
-def test_in_class_tuple(x: type[ClassA] | type[ClassB] | type[ClassC]):
-    if x in (ClassA, ClassB):
-        assert_type(x, type[ClassA] | type[ClassB])
+class NonFinalClassA: pass
+class SubA(NonFinalClassA): pass
+
+
+def test_in_final_class_tuple(x: type[FinalClassA] | type[FinalClassB] | type[ClassC]):
+    if x in (FinalClassA, FinalClassB):
+        assert_type(x, type[FinalClassA] | type[FinalClassB])
     else:
         assert_type(x, type[ClassC])
 
-def test_not_in_class_tuple(x: type[ClassA] | type[ClassB] | type[ClassC]):
-    if x not in (ClassA, ClassB):
+def test_not_in_final_class_tuple(x: type[FinalClassA] | type[FinalClassB] | type[ClassC]):
+    if x not in (FinalClassA, FinalClassB):
         assert_type(x, type[ClassC])
     else:
-        assert_type(x, type[ClassA] | type[ClassB])
+        assert_type(x, type[FinalClassA] | type[FinalClassB])
+
+def test_not_in_non_final_class_tuple(x: type[NonFinalClassA] | type[ClassC]):
+    if x not in (NonFinalClassA,):
+        # SubA is a subclass of NonFinalClassA. At runtime, SubA in (NonFinalClassA,)
+        # evaluates to False, so SubA reaches this negative branch. Therefore,
+        # type[NonFinalClassA] must not be eliminated when NonFinalClassA is not final.
+        assert_type(x, type[NonFinalClassA] | type[ClassC])
+    else:
+        assert_type(x, type[NonFinalClassA])
