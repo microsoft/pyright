c16836d3bfed85a04dd60e30a6e9f2951787294e
Henry Su <henrysu4707@gmail.com>
2026-09-24 16:59:24 -0500

    Add Sentinel coverage for member access, locals, aliasing and reassignment

--- packages/pyright-internal/src/tests/samples/sentinel3.py
+++ packages/pyright-internal/src/tests/samples/sentinel3.py
@@ -42,3 +42,39 @@ def update_not_missing(address: Address, email: str | MISSING) -> None:
     if email is not MISSING:
         address.email = email
         reveal_type(email, expected_text="str")
+
+
+class ClassA:
+    def __init__(self) -> None:
+        self.missing = Sentinel("missing")
+
+
+def func3(a: ClassA) -> None:
+    reveal_type(a.missing, expected_text="missing")
+
+    if a.missing is Empty:
+        reveal_type(a.missing, expected_text="Never")
+
+
+def func4() -> None:
+    local_empty = Sentinel("local_empty")
+    reveal_type(local_empty, expected_text="local_empty")
+
+    alias = Empty
+    reveal_type(alias, expected_text="Empty")
+
+    local_empty = 3
+    reveal_type(local_empty, expected_text="Literal[3]")
+
+
+Reassigned = Sentinel("Reassigned")
+Reassigned = 3
+
+
+def func5(value: int | Empty | MISSING) -> None:
+    reveal_type(Reassigned, expected_text="int | Reassigned")
+    alias = Empty
+    if value is alias:
+        reveal_type(value, expected_text="Empty")
+    else:
+        reveal_type(value, expected_text="int | MISSING")
